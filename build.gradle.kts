d6d465c72d3ebed581d44a66285ede26e0c9e66e
breandan <bre@ndan.co>
2018-12-08 23:59:31 -0500

    add test and fix toString impl

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,15 +22,16 @@ val spekVersion = "2.0.0-rc.1"
 dependencies {
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
 
-  testImplementation("org.spekframework.spek2:spek-dsl-jvm:2.0.0-alpha.1") {
-    exclude(group = "org.jetbrains.kotlin")
-  }
-  testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:2.0.0-alpha.1") {
-    exclude(group = "org.junit.platform")
-    exclude(group = "org.jetbrains.kotlin")
-  }
+//  testImplementation("org.spekframework.spek2:spek-dsl-jvm:2.0.0-alpha.1") {
+//    exclude(group = "org.jetbrains.kotlin")
+//  }
+//  testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:2.0.0-alpha.1") {
+//    exclude(group = "org.junit.platform")
+//    exclude(group = "org.jetbrains.kotlin")
+//  }
 
-  testRuntimeOnly("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+  testCompile("io.kotlintest:kotlintest-runner-junit5:3.1.10")
+//  testRuntimeOnly("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
 }
 
 tasks.withType<Test> {
