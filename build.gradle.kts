8e2c85ee1c97f52bc77e925e3fe85d0efd2daf5b
breandan <bre@ndan.co>
2018-12-10 00:11:32 -0500

    fix tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,11 @@ repositories.jcenter()
 
 val kotlinVersion = "1.3.11"
 
+val test by tasks.getting(Test::class) {
+  useJUnitPlatform()
+}
+
 dependencies {
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
-  testCompile("io.kotlintest:kotlintest-runner-junit5:3.1.10")
+  testCompile("io.kotlintest:kotlintest-runner-junit5:3.1.11")
 }
