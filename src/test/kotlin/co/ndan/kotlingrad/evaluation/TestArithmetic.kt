cb114c906d833f399ba904ba4a88f8c0e3b8a092
breandan.considine <breandan.considine@gmail.com>
2018-12-13 23:00:45 -0500

    more tests, prefer symbolic form to findiff

--- src/test/kotlin/co/ndan/kotlingrad/evaluation/TestArithmetic.kt
+++ src/test/kotlin/co/ndan/kotlingrad/evaluation/TestArithmetic.kt
@@ -1,13 +1,12 @@
 package co.ndan.kotlingrad.evaluation
 
 import co.ndan.kotlingrad.calculus.DoubleVarGenerator
-import co.ndan.kotlingrad.math.calculus.DoubleFunctor.sin
 import co.ndan.kotlingrad.math.types.Double
 import co.ndan.kotlingrad.math.types.Var
 import io.kotlintest.properties.assertAll
 import io.kotlintest.specs.StringSpec
 
-class TestArithmetic: StringSpec({
+class TestArithmetic : StringSpec({
   "test addition" {
     assertAll(DoubleVarGenerator, DoubleVarGenerator) { x: Var<Double>, y: Var<Double> ->
       (x + y).value == x.value + y.value
@@ -20,6 +19,12 @@ class TestArithmetic: StringSpec({
     }
   }
 
+  "test unary minus" {
+    assertAll(DoubleVarGenerator, DoubleVarGenerator) { x: Var<Double>, y: Var<Double> ->
+      (-y + x).value == x.value - y.value
+    }
+  }
+
   "test multiplication" {
     assertAll(DoubleVarGenerator, DoubleVarGenerator) { x: Var<Double>, y: Var<Double> ->
       (x * y).value == x.value * y.value
@@ -27,15 +32,20 @@ class TestArithmetic: StringSpec({
   }
 
   "test multiplication with numerical type" {
-    assertAll(DoubleVarGenerator) { x: Var<Double> ->
-      sin(x * 2) * 2
-      (x * 2).value == x.value * 2
+    assertAll(DoubleVarGenerator) {
+      (it * 2).value == it.value * 2
     }
   }
 
   "test division" {
     assertAll(DoubleVarGenerator, DoubleVarGenerator) { x: Var<Double>, y: Var<Double> ->
       (x / y).value == x.value / y.value
+    }
+  }
+
+  "test inverse" {
+    assertAll(DoubleVarGenerator, DoubleVarGenerator) { x: Var<Double>, y: Var<Double> ->
+      (x * y.inverse()).value == (x / y).value
     }
   }
 })
