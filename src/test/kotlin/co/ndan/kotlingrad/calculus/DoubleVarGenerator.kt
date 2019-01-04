3ef66f69683751039d1e87df96dde9642f7166eb
breandan <bre@ndan.co>
2019-01-03 23:15:13 -0500

    update tests and improve notation

--- src/test/kotlin/co/ndan/kotlingrad/calculus/DoubleVarGenerator.kt
+++ src/test/kotlin/co/ndan/kotlingrad/calculus/DoubleVarGenerator.kt
@@ -1,13 +1,9 @@
 package co.ndan.kotlingrad.calculus
 
-import co.ndan.kotlingrad.math.algebra.DoublePrototype
 import co.ndan.kotlingrad.math.numerical.Double
-import co.ndan.kotlingrad.math.types.Var
 import io.kotlintest.properties.Gen
 
-object DoubleVarGenerator: Gen<Var<Double>> {
-  override fun constants() = listOf(Var("", Double(0), DoublePrototype))
-  override fun random(): Sequence<Var<Double>> = generateSequence {
-    Var("", Double(Gen.double().random().first()), DoublePrototype)
-  }
+object DoubleGenerator: Gen<Double> {
+  override fun constants() = listOf(Double(0))
+  override fun random(): Sequence<Double> = generateSequence { Double(Gen.double().random().first()) }
 }
