b3aba3e89343129693837ce1da5c43cb8070443f
breandan <bre@ndan.co>
2019-01-07 00:45:18 -0500

    simplify notation a bit

--- src/main/kotlin/edu/umontreal/kotlingrad/math/samples/HelloKotlinGrad.kt
+++ src/main/kotlin/edu/umontreal/kotlingrad/math/samples/HelloKotlinGrad.kt
@@ -1,7 +1,6 @@
 package edu.umontreal.kotlingrad.math.samples
 
 import edu.umontreal.kotlingrad.math.calculus.DoubleFunctor
-import edu.umontreal.kotlingrad.math.numerical.DoubleReal
 
 @Suppress("NonAsciiCharacters", "LocalVariableName")
 fun main(args: Array<String>) {
@@ -16,9 +15,9 @@ fun main(args: Array<String>) {
     val `∂²z_∂x∂y` = d(`∂z_∂x`) / d(y) // Higher order partials
     val `∇z` = z.grad()                // Gradient operator
 
-    val values = mapOf(x to DoubleReal(0), y to DoubleReal(1))
+    val values = mapOf(x to 0, y to 1)
     val indVar = z.independentVariables().joinToString(", ")
-    val p = "${x(x to DoubleReal(0))}, ${y(y to DoubleReal(1))}"
+    val p = "${x(x to 0)}, ${y(y to 1)}"
     print("z($indVar) \t\t\t\t= $z\n" +
         "∂z($p)/∂x \t\t= $`∂z_∂x` \n\t\t\t\t\t\t= " + `∂z_∂x`(values) + "\n" +
         "∂z($p)/∂y \t\t= $`∂z_∂y` \n\t\t\t\t\t\t= " + `∂z_∂y`(values) + "\n" +
