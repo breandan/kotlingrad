951c643396b9f42dc1f7680b4290334c52f32027
breandan <bre@ndan.co>
2019-01-10 00:15:21 -0500

    update readme

--- src/main/kotlin/edu/umontreal/kotlingrad/samples/HelloKotlinGrad.kt
+++ src/main/kotlin/edu/umontreal/kotlingrad/samples/HelloKotlinGrad.kt
@@ -18,13 +18,12 @@ fun main(args: Array<String>) {
     val values = mapOf(x to 0, y to 1)
     val indVar = z.variables.joinToString(", ")
 
-    val p = "$values"
     print("z($indVar) \t\t\t= $z\n" +
-        "z($p) \t\t\t= ${z(values)}\n" +
-        "∂z($p)/∂x \t\t= $`∂z_∂x` \n\t\t\t\t= " + `∂z_∂x`(values) + "\n" +
-        "∂z($p)/∂y \t\t= $`∂z_∂y` \n\t\t\t\t= " + `∂z_∂y`(values) + "\n" +
-        "∂²z($p)/∂x² \t\t= $`∂z_∂y` \n\t\t\t\t= " + `∂²z_∂x²`(values) + "\n" +
-        "∂²z($p)/∂x∂y \t\t= $`∂²z_∂x∂y` \n\t\t\t\t= " + `∂²z_∂x∂y`(values) + "\n" +
-        "∇z($p) \t\t\t= $`∇z` \n\t\t\t\t= [${`∇z`[x]!!(values)}, ${`∇z`[y]!!(values)}]ᵀ")
+        "z($values) \t\t\t= ${z(values)}\n" +
+        "∂z($values)/∂x \t\t= $`∂z_∂x` \n\t\t\t\t= " + `∂z_∂x`(values) + "\n" +
+        "∂z($values)/∂y \t\t= $`∂z_∂y` \n\t\t\t\t= " + `∂z_∂y`(values) + "\n" +
+        "∂²z($values)/∂x² \t\t= $`∂z_∂y` \n\t\t\t\t= " + `∂²z_∂x²`(values) + "\n" +
+        "∂²z($values)/∂x∂y \t\t= $`∂²z_∂x∂y` \n\t\t\t\t= " + `∂²z_∂x∂y`(values) + "\n" +
+        "∇z($values) \t\t\t= $`∇z` \n\t\t\t\t= [${`∇z`[x]!!(values)}, ${`∇z`[y]!!(values)}]ᵀ")
   }
 }
