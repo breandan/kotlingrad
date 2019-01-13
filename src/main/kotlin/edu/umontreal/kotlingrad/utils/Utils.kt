5d7b01a09284fc1ec78135ab80ad5aa069f29f70
breandan <bre@ndan.co>
2019-01-13 14:24:31 -0500

    fix typo

--- src/main/kotlin/edu/umontreal/kotlingrad/utils/Utils.kt
+++ src/main/kotlin/edu/umontreal/kotlingrad/utils/Utils.kt
@@ -5,7 +5,7 @@ infix fun ClosedRange<Double>.step(step: Double): Iterable<Double> {
   require(endInclusive.isFinite())
   require(step > 0.0) { "Step must be positive, was: $step." }
   val sequence = generateSequence(start) { previous ->
-    if (previous == Double.POSITIVE∕INFINITY) return@generateSequence null
+    if (previous == Double.POSITIVE_INFINITY) return@generateSequence null
     val next = previous + step
     if (next > endInclusive) null else next
   }
