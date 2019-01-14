d5ce102f7b0164666d83f65c1eca840ff1281f13
breandan <bre@ndan.co>
2019-01-14 15:58:07 -0500

    refactor readme and add more documentation around testing

--- src/test/kotlin/edu/umontreal/kotlingrad/calculus/TestPartialDerivatives.kt
+++ src/test/kotlin/edu/umontreal/kotlingrad/calculus/TestPartialDerivatives.kt
@@ -11,42 +11,42 @@ class TestPartialDerivatives: StringSpec({
     val y = variable("y")
 
     "∂x / ∂y should be 0" {
-      assertAll(NumericalGenerator, NumericalGenerator) { xVal, yVal ->
+      assertAll(NumericalGenerator, NumericalGenerator) { ẋ, ẏ ->
         val f = cos(x)
         val `∂f∕∂y` = d(f) / d(y)
-        `∂f∕∂y`(x to xVal, y to yVal) + 0.0 shouldBeAbout 0.0
+        `∂f∕∂y`(x to ẋ, y to ẏ) + 0.0 shouldBeAbout 0.0
       }
     }
 
     "∂(x + y) / ∂x should be 1" {
-      assertAll(NumericalGenerator, NumericalGenerator) { xVal, yVal ->
+      assertAll(NumericalGenerator, NumericalGenerator) { ẋ, ẏ ->
         val f = x + y
         val `∂f∕∂x` = d(f) / d(x)
-        `∂f∕∂x`(x to xVal, y to yVal) shouldBeAbout 1.0
+        `∂f∕∂x`(x to ẋ, y to ẏ) shouldBeAbout 1.0
       }
     }
 
     "∂(x + y + x) / ∂x should be 2" {
-      assertAll(NumericalGenerator, NumericalGenerator) { xVal, yVal ->
+      assertAll(NumericalGenerator, NumericalGenerator) { ẋ, ẏ ->
         val f = x + y + x
         val `∂f∕∂x` = d(f) / d(x)
-        `∂f∕∂x`(x to xVal, y to yVal) shouldBeAbout 2.0
+        `∂f∕∂x`(x to ẋ, y to ẏ) shouldBeAbout 2.0
       }
     }
 
     "∂(yx) / ∂x should be y" {
-      assertAll(NumericalGenerator, NumericalGenerator) { xVal, yVal ->
+      assertAll(NumericalGenerator, NumericalGenerator) { ẋ, ẏ ->
         val f = y * x
         val `∂f∕∂x` = d(f) / d(x)
-        `∂f∕∂x`(x to xVal, y to yVal) shouldBeAbout yVal
+        `∂f∕∂x`(x to ẋ, y to ẏ) shouldBeAbout ẏ
       }
     }
 
     "∂(yx + xx + yy) / ∂x should be y + 2x" {
-      assertAll(NumericalGenerator, NumericalGenerator) { xVal, yVal ->
+      assertAll(NumericalGenerator, NumericalGenerator) { ẋ, ẏ ->
         val f = y * x + pow(x, 2) + pow(y, 2)
         val `∂f∕∂x` = d(f) / d(x)
-        `∂f∕∂x`(x to xVal, y to yVal) shouldBeAbout (y + x * 2)(x to xVal, y to yVal)
+        `∂f∕∂x`(x to ẋ, y to ẏ) shouldBeAbout (y + x * 2)(x to ẋ, y to ẏ)
       }
     }
   }
