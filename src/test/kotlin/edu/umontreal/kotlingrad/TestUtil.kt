45ad8197181207351eef0e1bc5aee52e9840d995
breandan <bre@ndan.co>
2019-01-22 21:18:27 -0500

    add new derivative test

--- src/test/kotlin/edu/umontreal/kotlingrad/TestUtil.kt
+++ src/test/kotlin/edu/umontreal/kotlingrad/TestUtil.kt
@@ -6,7 +6,7 @@ import io.kotlintest.shouldBe
 @Suppress("NonAsciiCharacters")
 val ε: Double = 1E-7
 
-infix fun Double.shouldBeAbout(d: Double) = if (isNaN() && d.isNaN()) Unit else this shouldBe (d plusOrMinus ε)
+infix fun Double.shouldBeAbout(d: Double) = if (isNaN() && d.isNaN() || d.isInfinite() && isInfinite()) Unit else this shouldBe (d plusOrMinus ε)
 
 infix fun Number.shouldBeAbout(d: Number) = this.toDouble() shouldBe d.toDouble()
 
--- src/test/kotlin/edu/umontreal/kotlingrad/calculus/TestSimpleDerivatives.kt
+++ src/test/kotlin/edu/umontreal/kotlingrad/calculus/TestSimpleDerivatives.kt
@@ -16,23 +16,33 @@ class TestSimpleDerivatives: StringSpec({
       assertAll(NumericalGenerator) { ẋ ->
         val f = x * 1
         val `∂f∕∂x` = d(f) / d(x)
-        `∂f∕∂x`(x to ẋ) shouldBeAbout 1
+        `∂f∕∂x`(ẋ) shouldBeAbout 1
       }
     }
 
     "d(2x) / dx should be 2" {
       assertAll(NumericalGenerator) { ẋ ->
         val f = x * 2
-        val `∂f∕∂x` = d(f) / d(x)
-        `∂f∕∂x`(x to ẋ) shouldBeAbout 2
+        val `df∕dx` = d(f) / d(x)
+        `df∕dx`(ẋ) shouldBeAbout 2
       }
     }
 
     "d(x + x) / dx should be 2" {
       assertAll(NumericalGenerator) { ẋ ->
         val f = x + x
-        val `∂f∕∂x` = d(f) / d(x)
-        `∂f∕∂x`(x to ẋ) shouldBeAbout 2
+        val `df∕dx` = d(f) / d(x)
+        `df∕dx`(ẋ) shouldBeAbout 2
+      }
+    }
+
+    "d(√x) / dx should be 1/(2√x)" {
+      assertAll(NumericalGenerator) { ẋ ->
+        val f1 = sqrt(x)
+        val f2 = pow(x, 0.5)
+        val `df1∕dx` = d(f1) / d(x)
+        val `df2∕dx` = d(f2) / d(x)
+        `df1∕dx`(ẋ) shouldBeAbout `df2∕dx`(ẋ)
       }
     }
   }
