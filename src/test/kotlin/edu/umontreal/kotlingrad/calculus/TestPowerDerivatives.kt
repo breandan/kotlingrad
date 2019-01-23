5242d30b28cc0533cf9cee6e567f30981461814d
breandan <bre@ndan.co>
2019-01-23 17:00:47 -0500

    power rule tests and generalize leibniz notation

--- src/test/kotlin/edu/umontreal/kotlingrad/calculus/TestPowerDerivatives.kt
+++ src/test/kotlin/edu/umontreal/kotlingrad/calculus/TestPowerDerivatives.kt
@@ -10,7 +10,7 @@ class TestPowerDerivatives: StringSpec({
   with(DoublePrecision) {
     val x = variable("x")
 
-    "d(1/x) / dx should be -1/x" {
+    "d(1/x) / dx should be -1/x²" {
       assertAll(NumericalGenerator) { ẋ ->
         val f1 = 1 / x
         val f2 = pow(x, -1)
@@ -28,6 +28,33 @@ class TestPowerDerivatives: StringSpec({
         val f1 = sqrt(x)
         val f2 = pow(x, 0.5)
         val manualDeriv = 1 / (2 * sqrt(x))
+        val `df1∕dx` = d(f1) / d(x)
+        val `df2∕dx` = d(f2) / d(x)
+        `df1∕dx`(ẋ) shouldBeAbout `df2∕dx`(ẋ)
+        `df1∕dx`(ẋ) shouldBeAbout manualDeriv(ẋ)
+        `df2∕dx`(ẋ) shouldBeAbout manualDeriv(ẋ)
+      }
+    }
+
+    "d(x¹) / dx should be 1" {
+      assertAll(NumericalGenerator) { ẋ ->
+        val f1 = x
+        val f2 = pow(x, 1)
+        val manualDeriv = 1
+        val `df1∕dx` = d(f1) / d(x)
+        val `df2∕dx` = d(f2) / d(x)
+        `df1∕dx`(ẋ) shouldBeAbout `df2∕dx`(ẋ)
+        `df1∕dx`(ẋ) shouldBeAbout manualDeriv(ẋ)
+        `df2∕dx`(ẋ) shouldBeAbout manualDeriv(ẋ)
+      }
+    }
+
+    "d(x⁰) / dx should be 0" {
+      assertAll(NumericalGenerator) { ẋ ->
+        println(ẋ)
+        val f1 = x / x
+        val f2 = pow(x, 0)
+        val manualDeriv = 0
         val `df1∕dx` = d(f1) / d(x)
         val `df2∕dx` = d(f2) / d(x)
         `df1∕dx`(ẋ) shouldBeAbout `df2∕dx`(ẋ)
