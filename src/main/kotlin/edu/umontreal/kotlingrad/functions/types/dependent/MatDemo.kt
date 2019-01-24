b0922af5195b4ff46847d9b5202bd8846b1e14ae
breandan <bre@ndan.co>
2019-01-24 08:58:57 -0500

    add type safe matrix multiplication demo

---
+++ src/main/kotlin/edu/umontreal/kotlingrad/functions/types/dependent/MatDemo.kt
@@ -0,0 +1,46 @@
+package edu.umontreal.kotlingrad.functions.types.dependent
+
+fun main(args: Array<String>) {
+  // Inferred type: Mat<Double, `1`, `3`>
+  val a = Mat(`1`, `3`, 1.0, 2.0, 3.0)
+  // Inferred type: Mat<Double, `3`, `1`>
+  val b = Mat(`3`, `1`, 1.0, 2.0, 3.0)
+  // Inferred type: Mat<Double, `1`, `1`>
+  val c = a * b
+
+// Does not compile, inner dimension mismatch
+//  a * a
+//  b * b
+
+// Does not compile, incompatible shape
+//  val b_ = Mat(`3`, `1`, 1.0, 2.0)
+
+// Does not compile, incompatible shape
+//  val b_ = Mat(`2`, `1`, 1.0, 2.0, 3.0)
+
+  // Inferred type: Mat<Double, `2`, `3`>
+  val d: Mat<Double, `2`, `3`> = Mat(`2`, `3`,
+    1.0, 2.0, 3.0,
+    4.0, 5.0, 6.0
+  )
+
+  // Inferred type: Mat<Double, `3`, `2`>
+  val e = Mat(`3`, `2`,
+    1.0, 2.0,
+    3.0, 4.0,
+    5.0, 6.0
+  )
+
+  // Inferred type: Mat<Double, `2`, `2`>
+  val f = d * e
+
+// Does not compile, inner dimension mismatch
+//  val g = e * b
+//  val h = f * b
+
+// Does not compile, incompatible size
+//  val d_: Mat<Double, `2`, `3`> = Mat(`2`, `3`,
+//    1.0, 2.0, 3.0,
+//    4.0, 5.0
+//  )
+}
---
+++ src/main/kotlin/edu/umontreal/kotlingrad/functions/types/dependent/Vec.kt
@@ -0,0 +1,84 @@
+package edu.umontreal.kotlingrad.functions.types.dependent
+
+// Supports vectors of up to length 6
+open class Vec<T, MaxLength: `4`> internal constructor(val contents: List<T> = arrayListOf()) {
+  companion object {
+    operator fun <T> invoke(): Vec<T, `0`> = Vec(arrayListOf())
+    operator fun <T> invoke(t: T): Vec<T, `1`> = Vec(arrayListOf(t))
+    operator fun <T> invoke(t0: T, t1: T): Vec<T, `2`> = Vec(arrayListOf(t0, t1))
+    operator fun <T> invoke(t0: T, t1: T, t2: T): Vec<T, `3`> = Vec(arrayListOf(t0, t1, t2))
+    operator fun <T> invoke(t0: T, t1: T, t2: T, t3: T): Vec<T, `4`> = Vec(arrayListOf(t0, t1, t2, t3))
+  }
+
+  override fun toString() = "$contents"
+}
+
+@JvmName("g1") infix operator fun <T> Vec<T, `1`>.get(i: `0`): T = contents[0]
+
+@JvmName("g2") infix operator fun <T> Vec<T, `2`>.get(i: `0`): T = contents[0]
+
+@JvmName("g2") infix operator fun <T> Vec<T, `2`>.get(i: `1`): T = contents[1]
+
+@JvmName("g3") infix operator fun <T> Vec<T, `3`>.get(i: `0`): T = contents[0]
+
+@JvmName("g3") infix operator fun <T> Vec<T, `3`>.get(i: `1`): T = contents[1]
+
+@JvmName("g3") infix operator fun <T> Vec<T, `3`>.get(i: `2`): T = contents[2]
+
+@JvmName("g4") infix operator fun <T> Vec<T, `4`>.get(i: `0`): T = contents[0]
+
+@JvmName("g4") infix operator fun <T> Vec<T, `4`>.get(i: `1`): T = contents[1]
+
+@JvmName("g4") infix operator fun <T> Vec<T, `4`>.get(i: `2`): T = contents[2]
+
+@JvmName("g4") infix operator fun <T> Vec<T, `4`>.get(i: `3`): T = contents[3]
+
+@JvmName("1p1") infix operator fun <C: `1`> Vec<Double, C>.plus(v: Vec<Double, C>): Vec<Double, C> = Vec(contents.zip(v.contents).map { it.first + it.second })
+
+@JvmName("2p2") infix operator fun <C: `2`> Vec<Double, C>.plus(v: Vec<Double, C>): Vec<Double, C> = Vec(contents.zip(v.contents).map { it.first + it.second })
+
+@JvmName("3p3") infix operator fun <C: `3`> Vec<Double, C>.plus(v: Vec<Double, C>): Vec<Double, C> = Vec(contents.zip(v.contents).map { it.first + it.second })
+
+@JvmName("4p4") infix operator fun <C: `4`> Vec<Double, C>.plus(v: Vec<Double, C>): Vec<Double, C> = Vec(contents.zip(v.contents).map { it.first + it.second })
+
+@JvmName("0cT") infix fun <T> Vec<T, `0`>.cat(t: T): Vec<T, `1`> = Vec(contents + t)
+
+@JvmName("1cT") infix fun <T> Vec<T, `1`>.cat(t: T): Vec<T, `2`> = Vec(contents + t)
+
+@JvmName("2cT") infix fun <T> Vec<T, `2`>.cat(t: T): Vec<T, `3`> = Vec(contents + t)
+
+@JvmName("3cT") infix fun <T> Vec<T, `3`>.cat(t: T): Vec<T, `4`> = Vec(contents + t)
+
+@JvmName("Tc0") infix fun <T> T.cat(t: Vec<T, `0`>): Vec<T, `1`> = Vec(arrayListOf(this))
+
+@JvmName("Tc1") infix fun <T> T.cat(t: Vec<T, `1`>): Vec<T, `2`> = Vec(arrayListOf(this) + t.contents)
+
+@JvmName("Tc2") infix fun <T> T.cat(t: Vec<T, `2`>): Vec<T, `3`> = Vec(arrayListOf(this) + t.contents)
+
+@JvmName("Tc3") infix fun <T> T.cat(t: Vec<T, `3`>): Vec<T, `4`> = Vec(arrayListOf(this) + t.contents)
+
+@JvmName("0c0") infix fun <T> Vec<T, `0`>.cat(l: Vec<T, `0`>): Vec<T, `0`> = l
+
+@JvmName("0c1") infix fun <T> Vec<T, `0`>.cat(l: Vec<T, `1`>): Vec<T, `1`> = l
+
+@JvmName("0c2") infix fun <T> Vec<T, `0`>.cat(l: Vec<T, `2`>): Vec<T, `2`> = l
+
+@JvmName("0c3") infix fun <T> Vec<T, `0`>.cat(l: Vec<T, `3`>): Vec<T, `3`> = l
+
+@JvmName("1c0") infix fun <T> Vec<T, `1`>.cat(l: Vec<T, `0`>): Vec<T, `1`> = this
+
+@JvmName("2c0") infix fun <T> Vec<T, `2`>.cat(l: Vec<T, `0`>): Vec<T, `2`> = this
+
+@JvmName("3c0") infix fun <T> Vec<T, `3`>.cat(l: Vec<T, `0`>): Vec<T, `3`> = this
+
+@JvmName("1c1") infix fun <T> Vec<T, `1`>.cat(l: Vec<T, `1`>): Vec<T, `2`> = Vec(contents + l.contents)
+
+@JvmName("1c2") infix fun <T> Vec<T, `1`>.cat(l: Vec<T, `2`>): Vec<T, `3`> = Vec(contents + l.contents)
+
+@JvmName("1c3") infix fun <T> Vec<T, `1`>.cat(l: Vec<T, `3`>): Vec<T, `4`> = Vec(contents + l.contents)
+
+@JvmName("2c1") infix fun <T> Vec<T, `2`>.cat(l: Vec<T, `1`>): Vec<T, `3`> = Vec(contents + l.contents)
+
+@JvmName("3c1") infix fun <T> Vec<T, `3`>.cat(l: Vec<T, `1`>): Vec<T, `4`> = Vec(contents + l.contents)
+
+@JvmName("2c2") infix fun <T> Vec<T, `2`>.cat(l: Vec<T, `2`>): Vec<T, `4`> = Vec(contents + l.contents)
