d7af4b26e1ba85f0910e2068225e6ac50a7fec58
breandan <bre@ndan.co>
2019-01-23 20:16:50 -0500

    increase FP precision

--- src/test/kotlin/edu/umontreal/kotlingrad/TestUtil.kt
+++ src/test/kotlin/edu/umontreal/kotlingrad/TestUtil.kt
@@ -4,14 +4,11 @@ import io.kotlintest.shouldBe
 import java.math.BigDecimal
 import java.math.MathContext
 
-@Suppress("NonAsciiCharacters")
-val ε: Int = 7
-
 infix fun Double.shouldBeAbout(d: Double) =
   if (isNaN() && d.isNaN() || d.isInfinite() && isInfinite()) Unit
   else round() shouldBe d.round()
 
-fun Double.round(precision: Int = ε) = BigDecimal(this, MathContext(precision)).toDouble()
+fun Double.round(precision: Int = 10) = BigDecimal(this, MathContext(precision)).toDouble()
 
 infix fun Number.shouldBeAbout(d: Number) = this.toDouble() shouldBe d.toDouble()
 
