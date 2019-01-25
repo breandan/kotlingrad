719037d7ec6c81eed8a81479a2db09c281c0076e
breandan <bre@ndan.co>
2019-01-25 10:10:26 -0500

    supports matrices up to 4x4, need to generate this

--- src/main/kotlin/edu/umontreal/kotlingrad/dependent/MatDemo.kt
+++ src/main/kotlin/edu/umontreal/kotlingrad/dependent/MatDemo.kt
@@ -14,7 +14,7 @@ fun main() {
 //  b * b
 
 // Does not compile, incompatible shape
-//  val b_ = Mat(`3`, `1`, 1.0, 2.0)
+  val b_ = Mat.invoke(`3`, `1`, 1.0, 2.0, 3.0, 4.0)
 
 // Does not compile, incompatible shape
 //  val b_ = Mat(`2`, `1`, 1.0)
@@ -67,4 +67,27 @@ fun main() {
 // Does not compile, inner dimension mismatch
 //  i * f
 //  i * d
+
+  // Inferred type: Mat<Double, `4`, `3`>
+  val l = Mat(`4`, `4`,
+    1.0, 2.0, 3.0, 4.0,
+    5.0, 6.0, 7.0, 8.0,
+    9.0, 0.0, 0.0, 0.0,
+    9.0, 0.0, 0.0, 0.0
+  )
+
+  // Inferred type: Mat<Double, `4`, `3`>
+  val m = Mat(`4`, `1`,
+    1.0,
+    2.0,
+    3.0,
+    4.0
+  )
+
+  // Inferred type: Mat<Double, `4`, `1`>
+  val n = l * m
+
+// Does not compile, inner dimension mismatch
+//  n * f
+//  n * d
 }
