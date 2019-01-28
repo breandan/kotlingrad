0bdc798ce23b8e73981162c7e2fa45f033dffcd3
breandan <bre@ndan.co>
2019-01-27 21:13:34 -0500

    add specialized matmul implementations

--- src/main/kotlin/edu/umontreal/kotlingrad/dependent/MatExt.kt
+++ src/main/kotlin/edu/umontreal/kotlingrad/dependent/MatExt.kt
@@ -1,7 +1,7 @@
 package edu.umontreal.kotlingrad.dependent
 
-@JvmName("doubleMatTranspose") fun <R: `9`, C: `9`, M: Mat<Double, R, C>> M.transpose() =
-  Mat<Double, C, R>(contents = (0 until numCols).map { i -> Vec(contents = contents.map { it[i] }, length = rowT) }, rowT = colT, colT = rowT)
+fun <T, R: `9`, C: `9`, M: Mat<T, R, C>> M.transpose() =
+  Mat<T, C, R>(contents = (0 until numCols).map { i -> Vec(contents = contents.map { it[i] }, length = rowT) }, rowT = colT, colT = rowT)
 
 @JvmName("longMatPlus") infix operator fun <R: `9`, C: `9`, M: Mat<Long, R, C>> M.plus(v: M): Mat<Long, R, C> = Mat<Long, R, C>(rowT, colT, contents.zip(v.contents).map { it.first + it.second })
 @JvmName("intMatPlus") infix operator fun <R: `9`, C: `9`, M: Mat<Int, R, C>> M.plus(v: M): Mat<Int, R, C> = Mat<Int, R, C>(rowT, colT, contents.zip(v.contents).map { it.first + it.second })
@@ -9,6 +9,33 @@ package edu.umontreal.kotlingrad.dependent
 @JvmName("byteMatPlus") infix operator fun <R: `9`, C: `9`, M: Mat<Byte, R, C>> M.plus(v: M): Mat<Byte, R, C> = Mat<Byte, R, C>(rowT, colT, contents.zip(v.contents).map { it.first + it.second })
 @JvmName("doubleMatPlus") infix operator fun <R: `9`, C: `9`, M: Mat<Double, R, C>> M.plus(v: M): Mat<Double, R, C> = Mat<Double, R, C>(rowT, colT, contents.zip(v.contents).map { it.first + it.second })
 @JvmName("floatMatPlus") infix operator fun <R: `9`, C: `9`, M: Mat<Float, R, C>> M.plus(v: M): Mat<Float, R, C> = Mat<Float, R, C>(rowT, colT, contents.zip(v.contents).map { it.first + it.second })
+
+@JvmName("longMatMinus") infix operator fun <R: `9`, C: `9`, M: Mat<Long, R, C>> M.minus(v: M): Mat<Long, R, C> = Mat<Long, R, C>(rowT, colT, contents.zip(v.contents).map { it.first - it.second })
+@JvmName("intMatMinus") infix operator fun <R: `9`, C: `9`, M: Mat<Int, R, C>> M.minus(v: M): Mat<Int, R, C> = Mat<Int, R, C>(rowT, colT, contents.zip(v.contents).map { it.first - it.second })
+@JvmName("shortMatMinus") infix operator fun <R: `9`, C: `9`, M: Mat<Short, R, C>> M.minus(v: M): Mat<Short, R, C> = Mat<Short, R, C>(rowT, colT, contents.zip(v.contents).map { it.first - it.second })
+@JvmName("byteMatMinus") infix operator fun <R: `9`, C: `9`, M: Mat<Byte, R, C>> M.minus(v: M): Mat<Byte, R, C> = Mat<Byte, R, C>(rowT, colT, contents.zip(v.contents).map { it.first - it.second })
+@JvmName("doubleMatMinus") infix operator fun <R: `9`, C: `9`, M: Mat<Double, R, C>> M.minus(v: M): Mat<Double, R, C> = Mat<Double, R, C>(rowT, colT, contents.zip(v.contents).map { it.first - it.second })
+@JvmName("floatMatMinus") infix operator fun <R: `9`, C: `9`, M: Mat<Float, R, C>> M.minus(v: M): Mat<Float, R, C> = Mat<Float, R, C>(rowT, colT, contents.zip(v.contents).map { it.first - it.second })
+
+@JvmName("longMatMult") infix operator fun <R1: `9`, C1: `9`, C2: `9`> Mat<Long, R1, C1>.times(v: Mat<Long, C1, C2>): Mat<Long, R1, C2> {
+  val m1 = Array(numRows) { LongArray(numCols) }
+  for (i in 0 until numRows)
+    for (j in 0 until numCols)
+      m1[i][j] = this[i][j]
+
+  val m2 = Array(v.numRows) { LongArray(v.numCols) }
+  for (i in 0 until v.numRows)
+    for (j in 0 until v.numCols)
+      m2[i][j] = v[i][j]
+
+  val mat = Array(numRows) { LongArray(v.numCols) }
+  for(i in 0 until numRows)
+    for(j in 0 until v.numCols)
+      for(k in 0 until v.numRows)
+        mat[i][j] += this[i][k] * m2[k][j]
+
+  return Mat(rowT, v.colT, mat.map { Vec(v.colT, it.toList()) })
+}
 
 @JvmName("intMatMult") infix operator fun <R1: `9`, C1: `9`, C2: `9`> Mat<Int, R1, C1>.times(v: Mat<Int, C1, C2>): Mat<Int, R1, C2> {
   val m1 = Array(numRows) { IntArray(numCols) }
@@ -30,6 +57,48 @@ package edu.umontreal.kotlingrad.dependent
   return Mat(rowT, v.colT, mat.map { Vec(v.colT, it.toList()) })
 }
 
+@JvmName("shortMatMult") infix operator fun <R1: `9`, C1: `9`, C2: `9`> Mat<Short, R1, C1>.times(v: Mat<Short, C1, C2>): Mat<Short, R1, C2> {
+  val m1 = Array(numRows) { ShortArray(numCols) }
+  for (i in 0 until numRows)
+    for (j in 0 until numCols)
+      m1[i][j] = this[i][j]
+
+  val m2 = Array(v.numRows) { ShortArray(v.numCols) }
+  for (i in 0 until v.numRows)
+    for (j in 0 until v.numCols)
+      m2[i][j] = v[i][j]
+
+  val mat = Array(numRows) { ShortArray(v.numCols) }
+  for(i in 0 until numRows)
+    for(j in 0 until v.numCols)
+      for(k in 0 until v.numRows)
+        mat[i][j] = (mat[i][j] + (this[i][k] * m2[k][j]).toShort()).toShort()
+
+
+  return Mat(rowT, v.colT, mat.map { Vec(v.colT, it.toList()) })
+}
+
+@JvmName("byteMatMult") infix operator fun <R1: `9`, C1: `9`, C2: `9`> Mat<Byte, R1, C1>.times(v: Mat<Byte, C1, C2>): Mat<Byte, R1, C2> {
+  val m1 = Array(numRows) { ByteArray(numCols) }
+  for (i in 0 until numRows)
+    for (j in 0 until numCols)
+      m1[i][j] = this[i][j]
+
+  val m2 = Array(v.numRows) { ByteArray(v.numCols) }
+  for (i in 0 until v.numRows)
+    for (j in 0 until v.numCols)
+      m2[i][j] = v[i][j]
+
+  val mat = Array(numRows) { ByteArray(v.numCols) }
+  for(i in 0 until numRows)
+    for(j in 0 until v.numCols)
+      for(k in 0 until v.numRows)
+        mat[i][j] = (mat[i][j] + (this[i][k] * m2[k][j]).toByte()).toByte()
+
+
+  return Mat(rowT, v.colT, mat.map { Vec(v.colT, it.toList()) })
+}
+
 @JvmName("doubleMatMult") infix operator fun <R1: `9`, C1: `9`, C2: `9`> Mat<Double, R1, C1>.times(v: Mat<Double, C1, C2>): Mat<Double, R1, C2> {
   val m1 = Array(numRows) { DoubleArray(numCols) }
   for (i in 0 until numRows)
@@ -42,6 +111,26 @@ package edu.umontreal.kotlingrad.dependent
       m2[i][j] = v[i][j]
 
   val mat = Array(numRows) { DoubleArray(v.numCols) }
+  for(i in 0 until numRows)
+    for(j in 0 until v.numCols)
+      for(k in 0 until v.numRows)
+        mat[i][j] += this[i][k] * m2[k][j]
+
+  return Mat(rowT, v.colT, mat.map { Vec(v.colT, it.toList()) })
+}
+
+@JvmName("floatMatMult") infix operator fun <R1: `9`, C1: `9`, C2: `9`> Mat<Float, R1, C1>.times(v: Mat<Float, C1, C2>): Mat<Float, R1, C2> {
+  val m1 = Array(numRows) { FloatArray(numCols) }
+  for (i in 0 until numRows)
+    for (j in 0 until numCols)
+      m1[i][j] = this[i][j]
+
+  val m2 = Array(v.numRows) { FloatArray(v.numCols) }
+  for (i in 0 until v.numRows)
+    for (j in 0 until v.numCols)
+      m2[i][j] = v[i][j]
+
+  val mat = Array(numRows) { FloatArray(v.numCols) }
   for(i in 0 until numRows)
     for(j in 0 until v.numCols)
       for(k in 0 until v.numRows)
