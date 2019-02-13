3351b77e1ba171448e68687bbb2db1f78085b36c
breandan <bre@ndan.co>
2019-02-13 14:58:23 -0500

    refactor

--- src/main/kotlin/edu/umontreal/kotlingrad/coroutines/Delimited.kt
+++ src/main/kotlin/edu/umontreal/kotlingrad/coroutines/Delimited.kt
@@ -13,77 +13,61 @@ import kotlin.coroutines.intrinsics.*
  * (* 2 (reset (+ 1 (shift k (k 5)))))
  * ```
  *
- * translates to:
+ * Translates to:
  *
  * ```
- * 2 * reset<Int> {
- *     1 + shift<Int> { k -> k(5) }
- * }
+ * 2 * reset<Int> { 1 + shift<Int> { k -> k(5) } }
  * ```
  */
+
 fun <T> reset(body: suspend DelimitedScope<T>.() -> T): T =
-    DelimitedScopeImpl<T>().also {
-        body.startCoroutine(it, it)
-    }.runReset()
+    DelimitedScopeImpl<T>().also { body.startCoroutine(it, it) }.runReset()
 
 interface DelimitedContinuation<T, R> {
-    fun invokeWith(value: Result<R>): T
+  fun invokeWith(value: Result<R>): T
 }
 
-operator fun <T, R> DelimitedContinuation<T, R>.invoke(value: R): T =
-    invokeWith(Result.success(value))
+operator fun <T, R> DelimitedContinuation<T, R>.invoke(value: R): T = invokeWith(Result.success(value))
 
 @RestrictsSuspension
-abstract class DelimitedScope<T> {
-    abstract suspend fun <R> shift(block: (DelimitedContinuation<T, R>) -> T): R
-}
+abstract class DelimitedScope<T> { abstract suspend fun <R> shift(block: (DelimitedContinuation<T, R>) -> T): R }
 
 private class DelimitedScopeImpl<T> : DelimitedScope<T>(), Continuation<T>, DelimitedContinuation<T, Any?> {
-    private var shifted: ((DelimitedContinuation<T, Any?>) -> T)? = null
-    private var cont: Continuation<Any?>? = null
-    private var result: Result<T>? = null
+  private var shifted: ((DelimitedContinuation<T, Any?>) -> T)? = null
+  private var cont: Continuation<Any?>? = null
+  private var result: Result<T>? = null
 
-    override val context: CoroutineContext
-        get() = EmptyCoroutineContext
+  override val context: CoroutineContext
+    get() = EmptyCoroutineContext
 
-    override fun resumeWith(result: Result<T>) {
-        this.result = result
-    }
+  override fun resumeWith(result: Result<T>) {
+    this.result = result
+  }
 
-    @Suppress("UNCHECKED_CAST")
-    override suspend fun <R> shift(block: (DelimitedContinuation<T, R>) -> T): R =
-        suspendCoroutineUninterceptedOrReturn {
-            shifted = block as ((DelimitedContinuation<T, Any?>) -> T)
-            cont = it as Continuation<Any?>
-            COROUTINE_SUSPENDED
-        }
+  @Suppress("UNCHECKED_CAST")
+  override suspend fun <R> shift(block: (DelimitedContinuation<T, R>) -> T): R =
+      suspendCoroutineUninterceptedOrReturn {
+        shifted = block as ((DelimitedContinuation<T, Any?>) -> T)
+        cont = it as Continuation<Any?>
+        COROUTINE_SUSPENDED
+      }
 
-    override fun invokeWith(value: Result<Any?>): T {
-        val cont = this.cont?.also { cont = null }
-            ?: error("Delimited continuation is single-shot and cannot be invoked twice")
-        cont.resumeWith(value)
-        return takeResult()
-    }
+  override fun invokeWith(value: Result<Any?>): T {
+    val cont = this.cont?.also { cont = null } ?: error("Delimited continuation is single-shot and cannot be invoked twice")
+    cont.resumeWith(value)
+    return takeResult()
+  }
 
-    operator fun invoke(value: Result<Any?>): T {
-        val cont = this.cont?.also { cont = null }
-            ?: error("Delimited continuation is single-shot and cannot be invoked twice")
-        cont.resumeWith(value)
-        return takeResult()
-    }
+  tailrec fun runReset(): T {
+    val shifted = shifted?.also { shifted = null } ?: return takeResult()
+    result = runCatching { shifted(this) }
+    return runReset()
+  }
 
-    fun runReset(): T {
-        while (true) {
-            val shifted = shifted?.also { shifted = null }
-                ?: return takeResult()
-            result = runCatching { shifted(this) }
-        }
-    }
-
-    private fun takeResult(): T {
-        val result = this.result
-        check(result != null)
-        this.result = null
-        return result.getOrThrow()
-    }
+  private fun takeResult(): T {
+    val result = this.result
+    check(result != null)
+    this.result = null
+    return result.getOrThrow()
+  }
 }
--- src/test/kotlin/edu/umontreal/kotlingrad/delimited/DelimitedTest.kt
+++ src/test/kotlin/edu/umontreal/kotlingrad/delimited/DelimitedTest.kt
@@ -1,9 +1,10 @@
 package edu.umontreal.kotlingrad.delimited
 
-import edu.umontreal.kotlingrad.coroutines.DelimitedContinuation
 import edu.umontreal.kotlingrad.coroutines.reset
 import junit.framework.Assert.assertEquals
 import org.junit.*
+import edu.umontreal.kotlingrad.coroutines.invoke
+
 
 class DelimitedTest {
   @Test
@@ -14,53 +15,42 @@ class DelimitedTest {
 
   @Test
   fun testShiftOnly() {
-    val x = reset<Int> {
-      shift<Int> { k -> k(42) }
-    }
+    val x = reset<Int> { shift<Int> { k -> k(42) } }
     assertEquals(42, x)
   }
 
   @Test
   fun testShiftRight() {
-    val x = reset<Int> {
-      40 + shift<Int> { k -> k(2) }
-    }
+    val x = reset<Int> { 40 + shift<Int> { k -> k(2) } }
     assertEquals(42, x)
   }
 
   @Test
   fun testShiftLeft() {
-    val x = reset<Int> {
-      shift<Int> { k: DelimitedContinuation<Int, Int> -> k(40) } + 2
-    }
+    val x = reset<Int> { shift<Int> { k -> k(40) } + 2 }
     assertEquals(42, x)
   }
 
   @Test
   fun testShiftBoth() {
-    val x = reset<Int> {
-      shift<Int> { k -> k(40) } +
-          shift<Int> { k -> k(2) }
-    }
+    val x = reset<Int> { shift<Int> { k -> k(40) } + shift<Int> { k -> k(2) } }
     assertEquals(42, x)
   }
 
   @Test
   fun testShiftToString() {
-    val x = reset<String> {
-      shift<Int> { k -> k(42) }.toString()
-    }
+    val x = reset<String> { shift<Int> { k -> k(42) }.toString() }
     assertEquals("42", x)
   }
+
+  // TODO: Is this possible? https://en.wikiversity.org/wiki/Introduction_to_Delimited_Continuations/Handling_the_continuation
 
   // From: https://en.wikipedia.org/wiki/Delimited_continuation
   // (* 2 (reset (+ 1 (shift k (k 5)))))
   // k := (+ 1 [])
   @Test
   fun testWikiSample() {
-    val x = 2 * reset<Int> {
-      1 + shift<Int> { k -> k(5) }
-    }
+    val x = 2 * reset<Int> { 1 + shift<Int> { k -> k(5) } }
     assertEquals(12, x)
   }
 }
