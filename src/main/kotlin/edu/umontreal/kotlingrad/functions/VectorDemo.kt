0363e4b9143bc53661bd6da8263bbbf1ba42ff62
breandan <bre@ndan.co>
2019-03-06 17:28:38 -0500

    vector demo works!

---
+++ src/main/kotlin/edu/umontreal/kotlingrad/functions/VectorDemo.kt
@@ -0,0 +1,17 @@
+package edu.umontreal.kotlingrad.functions
+
+import edu.umontreal.kotlingrad.numerical.DoublePrecision
+
+//infix operator fun <Y: `100`, X: Field<X>> Number.times(fn: VectorFun<X, Y>): VectorFun<X, Y> = fn * ScalarConst(wrap(this))
+//infix fun <Y: `100`, X: Field<X>> ScalarFun<X>.mult(fn: VectorFun<X, Y>): VectorFun<X, Y> = fn * this
+
+fun main() {
+  with(DoublePrecision) {
+    val y = Var("y")
+
+    val t = VectorFun(1 * y, 2 * y, 3 * y)
+    val z = VectorFun(1 * y, 2 * y, 3 * y)
+    val m = t * z * y
+    val q = z * 3
+  }
+}
