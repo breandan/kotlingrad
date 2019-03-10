fbe51662d33270a50a3a8475fb0cd1bab3c77c02
breandan <bre@ndan.co>
2019-03-10 00:45:58 -0500

    VectorFun improvements

--- src/main/kotlin/edu/umontreal/kotlingrad/functions/VectorFun.kt
+++ src/main/kotlin/edu/umontreal/kotlingrad/functions/VectorFun.kt
@@ -6,41 +6,31 @@ import edu.umontreal.kotlingrad.dependent.*
 import java.util.*
 
 // VFun should not be a List or the concatenation operator + will conflict with vector addition
-open class VectorFun<X: Field<X>, MaxLength: `100`>(
-  val length: Nat<MaxLength>,
-  private val contents: ArrayList<X>,
-  override val variables: Set<Var<X>> = emptySet()):
-  AbelianGroup<VectorFun<X, MaxLength>>, Function<X>, List<X> by contents {
-
-  constructor(fn: VectorFun<X, MaxLength>): this(fn.length, fn.contents, fn.variables)
-  //TODO: Fix this
-  constructor(vararg fns: VectorFun<X, MaxLength>): this(fns[0].length, fns[0].contents, fns.flatMap { it.variables }.toSet())
-
-  init {
-    if (length.i != contents.size) throw IllegalArgumentException("Declared $length, but found ${contents.size}")
-  }
+open class VectorFun<X: Field<X>, MaxLength: `100`> protected constructor(
+  open val length: Nat<MaxLength>,
+  val variables: Set<Var<X>> = emptySet()):
+  AbelianGroup<VectorFun<X, MaxLength>> {
 
   companion object {
-    operator fun <T: Field<T>> invoke(): VectorFun<T, `0`> = VectorFun(`0`, arrayListOf())
-    operator fun <T: Field<T>> invoke(t: T): VectorFun<T, `1`> = VectorFun(`1`, arrayListOf(t))
-    operator fun <T: Field<T>> invoke(t0: T, t1: T): VectorFun<T, `2`> = VectorFun(`2`, arrayListOf(t0, t1))
-    operator fun <T: Field<T>> invoke(t0: T, t1: T, t2: T): VectorFun<T, `3`> = VectorFun(`3`, arrayListOf(t0, t1, t2))
-    operator fun <T: Field<T>> invoke(t0: T, t1: T, t2: T, t3: T): VectorFun<T, `4`> = VectorFun(`4`, arrayListOf(t0, t1, t2, t3))
-    operator fun <T: Field<T>> invoke(t0: T, t1: T, t2: T, t3: T, t4: T): VectorFun<T, `5`> = VectorFun(`5`, arrayListOf(t0, t1, t2, t3, t4))
-    operator fun <T: Field<T>> invoke(t0: T, t1: T, t2: T, t3: T, t4: T, t5: T): VectorFun<T, `6`> = VectorFun(`6`, arrayListOf(t0, t1, t2, t3, t4, t5))
-    operator fun <T: Field<T>> invoke(t0: T, t1: T, t2: T, t3: T, t4: T, t5: T, t6: T): VectorFun<T, `7`> = VectorFun(`7`, arrayListOf(t0, t1, t2, t3, t4, t5, t6))
-    operator fun <T: Field<T>> invoke(t0: T, t1: T, t2: T, t3: T, t4: T, t5: T, t6: T, t7: T): VectorFun<T, `8`> = VectorFun(`8`, arrayListOf(t0, t1, t2, t3, t4, t5, t6, t7))
-    operator fun <T: Field<T>> invoke(t0: T, t1: T, t2: T, t3: T, t4: T, t5: T, t6: T, t7: T, t8: T): VectorFun<T, `9`> = VectorFun(`9`, arrayListOf(t0, t1, t2, t3, t4, t5, t6, t7, t8))
+    operator fun <T: Field<T>> invoke(): VectorConst<T, `0`> = VectorConst(`0`, arrayListOf())
+    operator fun <T: Field<T>> invoke(t: T): VectorConst<T, `1`> = VectorConst(`1`, arrayListOf(t))
+    operator fun <T: Field<T>> invoke(t0: T, t1: T): VectorConst<T, `2`> = VectorConst(`2`, arrayListOf(t0, t1))
+    operator fun <T: Field<T>> invoke(t0: T, t1: T, t2: T): VectorConst<T, `3`> = VectorConst(`3`, arrayListOf(t0, t1, t2))
+    operator fun <T: Field<T>> invoke(t0: T, t1: T, t2: T, t3: T): VectorConst<T, `4`> = VectorConst(`4`, arrayListOf(t0, t1, t2, t3))
+    operator fun <T: Field<T>> invoke(t0: T, t1: T, t2: T, t3: T, t4: T): VectorConst<T, `5`> = VectorConst(`5`, arrayListOf(t0, t1, t2, t3, t4))
+    operator fun <T: Field<T>> invoke(t0: T, t1: T, t2: T, t3: T, t4: T, t5: T): VectorConst<T, `6`> = VectorConst(`6`, arrayListOf(t0, t1, t2, t3, t4, t5))
+    operator fun <T: Field<T>> invoke(t0: T, t1: T, t2: T, t3: T, t4: T, t5: T, t6: T): VectorConst<T, `7`> = VectorConst(`7`, arrayListOf(t0, t1, t2, t3, t4, t5, t6))
+    operator fun <T: Field<T>> invoke(t0: T, t1: T, t2: T, t3: T, t4: T, t5: T, t6: T, t7: T): VectorConst<T, `8`> = VectorConst(`8`, arrayListOf(t0, t1, t2, t3, t4, t5, t6, t7))
+    operator fun <T: Field<T>> invoke(t0: T, t1: T, t2: T, t3: T, t4: T, t5: T, t6: T, t7: T, t8: T): VectorConst<T, `9`> = VectorConst(`9`, arrayListOf(t0, t1, t2, t3, t4, t5, t6, t7, t8))
   }
 
-  override fun toString() = "$contents"
-  override val one: VectorFun<X, MaxLength>
-    get() = VectorFun(length, mapTo(ArrayList(size)) { contents.first().one })
-  override val zero: VectorFun<X, MaxLength>
-    get() = VectorFun(length, mapTo(ArrayList(size)) { contents.first().zero })
+  override val one: VectorConst<X, MaxLength>
+    get() = VectorConst(length, (0..length.i).map { this().first().one })
+  override val zero: VectorConst<X, MaxLength>
+    get() = VectorConst(length, (0..length.i).map { this().first().zero })
 
-  constructor(length: Nat<MaxLength>, vector: Collection<X>): this(length, vector.mapTo(ArrayList<X>(vector.size)) { it })
-  constructor(length: Nat<MaxLength>, vararg vector: X): this(length, arrayListOf(*vector))
+//  private constructor(length: Nat<MaxLength>, vector: Collection<X>): this(length, vector.mapTo(ArrayList<X>(vector.size)) { it })
+//  private constructor(length: Nat<MaxLength>, vararg vector: X): this(length, arrayListOf(*vector))
 
 //  override operator fun invoke(map: Map<Var<X>, X>) = when (this) {
 //    is VectorProduct -> merge(multiplicand) { it.first(map) * it.second(map) }
@@ -48,28 +38,68 @@ open class VectorFun<X: Field<X>, MaxLength: `100`>(
 //    else -> throw Exception("Unknown")
 //  }
 
-  override fun unaryMinus() = broadcast { -it }
+  open operator fun invoke(map: Map<Var<X>, X> = emptyMap()): VectorConst<X, MaxLength> = when (this) {
+    is VectorSum<X, MaxLength> -> augend(map) + addend(map)
+    is VectorProduct<X, MaxLength> -> multiplicator(map) * multiplicand(map)
+//    is VectorDotProduct<X, MaxLength> -> multiplicator(map) dot multiplicand(map)
+    is VectorNeg<X, MaxLength> -> argument(map)
+    else -> TODO()
+  }
+
+  override fun unaryMinus() = VectorNeg(this)
   override fun plus(addend: VectorFun<X, MaxLength>) = VectorSum(this, addend)
-  override fun minus(subtrahend: VectorFun<X, MaxLength>) = merge(subtrahend) { it.first - it.second }
+  override fun minus(subtrahend: VectorFun<X, MaxLength>) = VectorSum(this, subtrahend)
   override fun times(multiplicand: VectorFun<X, MaxLength>) = VectorProduct(this, multiplicand)
-  infix fun dot(vx: VectorFun<X, MaxLength>) = (this * vx).reduce { acc, it -> acc + it }
+  infix fun dot(multiplicand: VectorFun<X, MaxLength>) = VectorDotProduct(this, multiplicand)
 
-  fun broadcast(operation: (X) -> X): VectorFun<X, MaxLength> = VectorFun(length, contents.map(operation))
-  private fun merge(vFun: VectorFun<X, MaxLength>, operation: (Pair<X, X>) -> X) = VectorFun(length, zip(vFun).map(operation))
+  override fun toString(): String = when (this) {
+    is VectorProduct -> "$multiplicator*$multiplicand"
+    is VectorSum -> "$augend+$addend"
+    is VectorDotProduct -> "$multiplicator+$multiplicand"
+    is VectorNeg-> "-$argument"
+    else -> "UNKNOWN"
+  }
+}
 
+class VectorNeg<X: Field<X>, MaxLength: `100`> internal constructor(
+  val argument: VectorFun<X, MaxLength>
+): VectorFun<X, MaxLength>(argument.length, argument.variables)
+
+class VectorProduct<X: Field<X>, MaxLength: `100`> internal constructor(
+  val multiplicator: VectorFun<X, MaxLength>,
+  val multiplicand: VectorFun<X, MaxLength>
+): VectorFun<X, MaxLength>(multiplicator.length, multiplicator.variables + multiplicand.variables)
+
+class VectorDotProduct<X: Field<X>, MaxLength: `100`> internal constructor(
+  val multiplicator: VectorFun<X, MaxLength>,
+  val multiplicand: VectorFun<X, MaxLength>
+): VectorFun<X, MaxLength>(multiplicator.length, multiplicator.variables + multiplicand.variables)
+
+class VectorSum<X: Field<X>, MaxLength: `100`> internal constructor(
+  val augend: VectorFun<X, MaxLength>,
+  val addend: VectorFun<X, MaxLength>
+): VectorFun<X, MaxLength>(augend.length, augend.variables + addend.variables)
+
+class VectorConst<X: Field<X>, MaxLength: `100`> internal constructor(
+  override val length: Nat<MaxLength>,
+  private val contents: List<X>): VectorFun<X, MaxLength>(length), List<X> by contents {
+  init {
+    if (length.i != contents.size) throw IllegalArgumentException("Declared $length, but found ${contents.size}")
+  }
+
+//  override operator fun invoke(map: Map<Var<X>, X>): VectorConst<X, MaxLength> = VectorConst(length, contents.map { it(map) })
+
+  operator fun plus(addend: VectorConst<X, MaxLength>): VectorConst<X, MaxLength>  = merge(addend) { it.first * it.second }
+  operator fun minus(subtrahend: VectorConst<X, MaxLength>): VectorConst<X, MaxLength>  = merge(subtrahend) { it.first - it.second }
+  operator fun times(multiplicand: VectorConst<X, MaxLength>): VectorConst<X, MaxLength> = merge(multiplicand) { it.first * it.second }
+  infix fun dot(vx: VectorConst<X, MaxLength>): X = (this * vx).reduce { acc, it -> acc + it }
+
+  private fun merge(vFun: VectorConst<X, MaxLength>, operation: (Pair<X, X>) -> X) = VectorConst(length, zip(vFun).map(operation))
+  fun broadcast(operation: (X) -> X): VectorConst<X, MaxLength> = VectorConst(length, contents.map(operation))
   infix operator fun plus(addend: X): VectorFun<X, MaxLength> = broadcast { it + addend }
   infix operator fun minus(subtrahend: X): VectorFun<X, MaxLength> = broadcast { it - subtrahend }
   infix operator fun times(multiplicand: X): VectorFun<X, MaxLength> = broadcast { it * multiplicand }
   infix operator fun div(divisor: X): VectorFun<X, MaxLength> = broadcast { it / divisor }
-//  class VConst<X : Field<X>, MaxLength: `100`>(override val length: Nat<MaxLength>, vararg contents: ScalarConst<X>) : VFun<X, MaxLength>(length, *contents)
+
+  override fun toString() = contents.toString()
 }
-
-class VectorProduct<X: Field<X>, MaxLength: `100`> internal constructor(
-  override val multiplicator: VectorFun<X, MaxLength>,
-  override val multiplicand: VectorFun<X, MaxLength>
-): VectorFun<X, MaxLength>(multiplicator, multiplicand), Product<X>, Function<X>
-
-class VectorSum<X: Field<X>, MaxLength: `100`> internal constructor(
-  override val augend: VectorFun<X, MaxLength>,
-  override val addend: VectorFun<X, MaxLength>
-): VectorFun<X, MaxLength>(augend, addend), Sum<X>
