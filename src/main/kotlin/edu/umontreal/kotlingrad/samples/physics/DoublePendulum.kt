d23699233cc8cfa6b771b6f9090e2152e05b1af5
breandan.considine <breandan.considine@gmail.com>
2019-10-18 00:57:23 -0400

    constant tuning with gradient descent

--- src/main/kotlin/edu/umontreal/kotlingrad/samples/physics/DoublePendulum.kt
+++ src/main/kotlin/edu/umontreal/kotlingrad/samples/physics/DoublePendulum.kt
@@ -17,54 +17,130 @@ import javafx.stage.Stage
 import javafx.util.Duration
 import kotlin.math.*
 
-
 class DoublePendulum(private val len: Double = 900.0) : Application(), EventHandler<ActionEvent> {
-  var a1 = 0.0
-  var a2 = 0.0
-  var ω1 = 0.0
-  var ω2 = 0.0
-  var α1 = 0.0
-  var α2 = 0.0
-  var m1 = 2.0
-  var m2 = 2.0
-
-  var G = 9.8
-  var µ = 0.02
-
-  var r1 = DoublePrecision.Vec(1.0, 0.0)
+  var ω1: Fun<DoubleReal> = DoublePrecision.wrap(0.0) // Angular velocities
+  var ω2: Fun<DoubleReal> = DoublePrecision.wrap(0.0)
+  val m1 = 2.0 // Masses
+  val m2 = 2.0
+  var G: Fun<DoubleReal> = DoublePrecision.wrap(9.81) // Gravity
+  var µ: Fun<DoubleReal> = DoublePrecision.wrap(0.01) // Friction
+  val Gp = 0.01  // Simulate measurement error
+  val µp = -0.01
+  var r1 = DoublePrecision.Vec(1.0, 0.0) // Polar vector
   var r2 = DoublePrecision.Vec(1.0, 0.0)
+  val observationSteps = 30
+  var priorVal = 5.0
+  fun step(obs: Fun<DoubleReal>? = null, groundTruth: Pair<VConst<DoubleReal, `2`>, VConst<DoubleReal, `2`>>? = null) = with(DoublePrecision) {
+    val isObserving = false
+//    val priorVal = if(G is Var) G.asDouble()
+    if (isObserving) {
+      G = Var("G")
+//      µ = Var("µ")
+    }
+
+    if (iter == observationSteps) println("\nGOING BLIND\n")
+
+    val dt = 0.01
+    val a1 = (r1.angle % (2.0 * PI)).let {
+      it + if (it < 0) 2 * PI else 0.0
+    } - 3 * PI / 2
+    val a2 = (r2.angle % (2.0 * PI)).let {
+      it + if (it < 0) 2 * PI else 0.0
+    } - 3 * PI / 2
+
+    val j1 = -G * (2.0 * m1 + m2) * sin(a1)
+    val j2 = -m2 * G * sin(a1 - 2 * a2)
+    val j3 = -2.0 * sin(a1 - a2) * m2
+    val j4 = ω2 * ω2 * r2.magn + ω1 * ω1 * r1.magn * cos(a1 - a2)
+    val cs = r1.magn * (2 * m1 + m2 - m2 * cos(2 * a1 - 2 * a2))
+    val α1 = (j1 + j2 + j3 * j4) * (1 / cs) - µ * ω1
+    ω1 += α1 * dt
+
+    val k1 = 2.0 * sin(a1 - a2)
+    val k2 = ω1 * ω1 * r1.magn * (m1 + m2)
+    val k3 = G * (m1 + m2) * cos(a1)
+    val k4 = ω2 * ω2 * r2.magn * m2 * cos(a1 - a2)
+    val ds = r2.magn * (2 * m1 + m2 - m2 * cos(2 * a1 - 2 * a2))
+    val α2 = k1 * (k2 + k3 + k4) * (1 / ds) + µ * ω2
+    ω2 -= α2 * dt
+
+    if(isObserving && obs != null) {
+      val loss = (ω2 - obs) pow 2
+      G = loss.descend(100, 0.0, 0.9, 0.1, G as Var<DoubleReal> to wrap(priorVal))
+      priorVal = G.asDouble()
+      println(G)
+    }
+
+    val r1a = (r1.angle + (ω1 * dt + .5 * α1 * dt * dt)).run {
+      if(G is Var) this(G as Var<DoubleReal> to priorVal).asDouble() else try { asDouble() } catch(e: Exception) {println(this); this(sVars.first() to priorVal).asDouble() }
+    }
+    val r2a = (r2.angle + - (ω2 * dt + .5 * α2 * dt * dt)).run {
+      if(G is Var) this(G as Var<DoubleReal> to priorVal).asDouble() else try { asDouble() } catch(e: Exception) {println(this); this(sVars.first() to priorVal).asDouble() }
+    }
+
+    if(G is Var) {
+      ω1 = ω1(G as Var<DoubleReal> to priorVal)
+      ω2 = ω2(G as Var<DoubleReal> to priorVal)
+    }
+
+    r1 = Vec(r1.magn * cos(r1a), r1.magn * sin(r1a))
+    r2 = Vec(r2.magn * cos(r2a), r2.magn * sin(r2a))
+
+    // Resync state while observing
+    if(isObserving) {
+      r1 = Vec(r1.magn * cos(r1a), r1.magn * sin(r1a))
+      r2 = Vec(r2.magn * cos(r2a), r2.magn * sin(r2a))
+    }
+  }
+
+  fun Fun<DoubleReal>.descend(steps: Int, vinit: Double, gamma: Double, α: Double = 0.1, map: Pair<Var<DoubleReal>, DoubleReal>): Fun<DoubleReal> {
+    with(DoublePrecision) {
+      val d_dg = this@descend.df(map.first)
+      var G1P: Fun<DoubleReal> = map.second
+      var velocity: Fun<DoubleReal> = wrap(vinit)
+      var i = 0
+      do {
+        velocity = gamma * velocity + d_dg(map.first to G1P) * α
+        G1P -= velocity
+        i++
+      } while (abs(velocity.asDouble()) > 0.00001 && i < steps)
+
+      return G1P
+    }
+  }
 
   val rod1 = Line(len, 0.0, len, 0.0).apply { strokeWidth = 3.0 }
   val rod2 = Line(len, 0.0, len, 0.0).apply { strokeWidth = 3.0 }
 
   val rodLen = len / 4
 
-  val anchor = Circle(10.0, BLACK).apply { layoutX = 450.0; layoutY = 200.0 }
+  val anchor = Circle(10.0, BLACK).apply { layoutX = 470.0; layoutY = 200.0 }
 
   val bob1 = Circle(m1 * 10.0, GREEN)
   val bob2 = Circle(m2 * 10.0, GREEN) // Ground truth
 
+  lateinit var twin: DoublePendulum
   lateinit var rod3: Line
   lateinit var rod4: Line
   lateinit var bob3: Circle
   lateinit var bob4: Circle           // Prediction
 
-  lateinit var twin: DoublePendulum
-
   lateinit var canvas: Pane
 
   override fun start(stage: Stage) {
-    twin = DoublePendulum()
+    twin = DoublePendulum().apply {
+      G += DoublePrecision.wrap(Gp) // Perturb gravity and friction
+      µ += DoublePrecision.wrap(µp)
+
+      // TODO: Erase these parameters -- should be fully learned
+//    µ = 0.0
+//    G = 0.0
+    }
+
     rod3 = twin.rod1
     rod4 = twin.rod2
     bob3 = Circle(twin.m1 * 10, RED)
     bob4 = Circle(twin.m2 * 10, RED)
-
-    twin.G += 0.1
-
-    // Erase these parameters -- should be learned
-//    twin.µ = 0.0
-//    twin.G = 0.0
 
     canvas = Pane().apply { children.addAll(anchor, rod3, rod4, bob3, bob4, rod1, rod2, bob1, bob2) }
 
@@ -77,30 +153,26 @@ class DoublePendulum(private val len: Double = 900.0) : Application(), EventHand
     Timeline(KeyFrame(Duration.millis(20.0), this)).apply { cycleCount = Timeline.INDEFINITE }.play()
   }
 
-  val VConst<DoubleReal, `2`>.x: Double
-    get() = DoublePrecision.run { this@x[0].asDouble() }
-  val VConst<DoubleReal, `2`>.y: Double
-    get() = DoublePrecision.run { this@y[1].asDouble() }
+  val VConst<DoubleReal, `2`>.r: Double
+    get() = DoublePrecision.run { this@r[0].asDouble() }
+  val VConst<DoubleReal, `2`>.theta: Double
+    get() = DoublePrecision.run { this@theta[1].asDouble() }
 
   val VConst<DoubleReal, `2`>.end: VConst<DoubleReal, `2`>
-    get() = DoublePrecision.run { Vec(this@end.x + rodLen * magn * cos(angle), this@end.y - rodLen * magn * sin(angle)) }
+    get() = DoublePrecision.run { Vec(this@end.r + rodLen * magn * cos(angle), this@end.theta - rodLen * magn * sin(angle)) }
 
   val VConst<DoubleReal, `2`>.magn: Double
     get() = DoublePrecision.run { magnitude().asDouble() }
 
   val VConst<DoubleReal, `2`>.angle: Double
-    get() = DoublePrecision.run { atan2(this@angle.y, this@angle.x) }
-
-  fun makeVec(magn: Double, angle: Double) =
-    DoublePrecision.run { Vec(magn * cos(angle), magn * sin(angle)) }
+    get() = DoublePrecision.run { atan2(this@angle.theta, this@angle.r) }
 
   fun VConst<DoubleReal, `2`>.render(rod: Line, xAdjust: Double = 0.0, yAdjust: Double = 0.0) {
-    with(DoublePrecision) {
-      rod.startX = this@render.x + xAdjust
-      rod.startY = this@render.y + yAdjust
-      rod.endX = this@render.end.x + xAdjust
-      rod.endY = this@render.end.y + yAdjust
-    }
+    rod.startX = this@render.r + xAdjust
+    rod.startY = this@render.theta + yAdjust
+    val end = this@render.end
+    rod.endX = end.r + xAdjust
+    rod.endY = end.theta + yAdjust
   }
 
   fun renderBobs() {
@@ -117,8 +189,9 @@ class DoublePendulum(private val len: Double = 900.0) : Application(), EventHand
   var iter = 0
 
   override fun handle(t: ActionEvent) {
-    update(0.01)
-    twin.update(0.01)
+    this.step()
+    twin.step(ω2, Pair(r1, r2))
+    if (iter > 0) canvas.children.removeAt(canvas.children.size - 1) // Clears previous text
 
     renderArms()
     tracePaths()
@@ -128,10 +201,9 @@ class DoublePendulum(private val len: Double = 900.0) : Application(), EventHand
   }
 
   private fun renderInfo() {
-    canvas.children.removeIf { it is Text }
-    val bob1Err = DoublePrecision.run { (r1.end - twin.r1.end).magnitude() * (1 / 200.0) }
-    val bob2Err = DoublePrecision.run { (r2.end - twin.r2.end).magnitude() * (1 / 200.0) }
-    canvas.children.add(Text("Iteration: $iter\nbob_1 error: $bob1Err\nbob_2 error: $bob2Err").apply { layoutX = 300.0; layoutY = 50.0; font = Font("Monospaced", 20.0) })
+    val bob1Err = DoublePrecision.run { ((r1.end - twin.r1.end).magnitude() * (1 / 200.0)) }.toString().subSequence(0, 5)
+    val bob2Err = DoublePrecision.run { ((r2.end - twin.r2.end).magnitude() * (1 / 200.0)) }.toString().subSequence(0, 5)
+    canvas.children.add(Text("Iteration: $iter\nbob_1 error: $bob1Err\nbob_2 error: $bob2Err\nG = $G, Ĝ = ${twin.G}\nµ = $µ, µ\u0302 = ${twin.µ}").apply { layoutX = 300.0; layoutY = 50.0; font = Font("Monospaced", 20.0) })
   }
 
   private fun renderArms() {
@@ -153,35 +225,6 @@ class DoublePendulum(private val len: Double = 900.0) : Application(), EventHand
     )
 
     while (canvas.children.size > 1000) canvas.children.remove(10, 12)
-  }
-
-  fun update(dt: Double) = with(DoublePrecision) {
-    a1 = (r1.angle % (2.0 * PI)).let {
-      it + if (it < 0) 2 * PI else 0.0
-    } - 3 * PI / 2
-    a2 = (r2.angle % (2.0 * PI)).let {
-      it + if (it < 0) 2 * PI else 0.0
-    } - 3 * PI / 2
-
-    var k1 = -G * (2.0 * m1 + m2) * sin(a1)
-    var k2 = -m2 * G * sin(a1 - 2 * a2)
-    var k3 = -2.0 * sin(a1 - a2) * m2
-    var k4 = ω2 * ω2 * r2.magn + ω1 * ω1 * r1.magn * cos(a1 - a2)
-    var ds = r1.magn * (2 * m1 + m2 - m2 * cos(2 * a1 - 2 * a2))
-    α1 = (k1 + k2 + k3 * k4) / ds - µ * ω1
-
-    k1 = 2.0 * sin(a1 - a2)
-    k2 = ω1 * ω1 * r1.magn * (m1 + m2)
-    k3 = G * (m1 + m2) * cos(a1)
-    k4 = ω2 * ω2 * r2.magn * m2 * cos(a1 - a2)
-    ds = r2.magn * (2 * m1 + m2 - m2 * cos(2 * a1 - 2 * a2))
-    α2 = k1 * (k2 + k3 + k4) / ds + µ * ω2
-
-    ω1 += α1 * dt
-    ω2 -= α2 * dt
-
-    r1 = makeVec(r1.magn, r1.angle + (ω1 * dt + .5 * α1 * dt * dt))
-    r2 = makeVec(r2.magn, r2.angle - (ω2 * dt + .5 * α2 * dt * dt))
   }
 }
 
