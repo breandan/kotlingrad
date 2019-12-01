dd200fe61ec704d873f7fabcb230584f35104ad7
breandan <bre@ndan.co>
2019-12-01 02:53:12 -0500

    showcase DFG

--- src/main/kotlin/edu/umontreal/kotlingrad/samples/VisualizeDFG.kt
+++ src/main/kotlin/edu/umontreal/kotlingrad/samples/VisualizeDFG.kt
@@ -5,30 +5,33 @@ import guru.nidi.graphviz.attribute.*
 import guru.nidi.graphviz.attribute.Rank.RankDir.LEFT_TO_RIGHT
 import guru.nidi.graphviz.engine.Format.SVG
 import java.awt.FlowLayout
+import java.io.File
 import javax.swing.*
 
 fun main() {
-    with(DoublePrecision) {
-        val t = (1 + x * 2 - 3 + y + z / y).d(y).d(x) + z / y * 3 - 2
-        t.render()
-    }
+  with(DoublePrecision) {
+    val t = (1 + x * 2 - 3 + y + z / y).d(y).d(x) + z / y * 3 - 2
+    t.render("src/main/resources/dataflow.svg")
+  }
 }
 
-fun Fun<*>.render() {
-    val image = graph(directed = true) {
-        edge["color" eq "black", Arrow.NORMAL]
+fun Fun<*>.render(filename: String? = null) {
+  val image = graph(directed = true) {
+    edge["color" eq "black", Arrow.NORMAL]
 
-        graph[Rank.dir(LEFT_TO_RIGHT)]
+    graph[Rank.dir(LEFT_TO_RIGHT)]
 
-        toGraph()
-    }.toGraphviz().render(SVG).toImage().let { image ->
+    toGraph()
+  }.toGraphviz().render(SVG).run {
+    if (filename == null)
+      toImage().let { image ->
         JFrame().apply {
-            contentPane.layout = FlowLayout()
-            contentPane.add(JLabel(ImageIcon(image)))
-            pack()
-            isVisible = true
+          contentPane.layout = FlowLayout()
+          contentPane.add(JLabel(ImageIcon(image)))
+          pack()
+          isVisible = true
         }
-    }
-//    }.toFile(File("example.svg"))
+      }
+    else toFile(File(filename))
+  }
 }
-
