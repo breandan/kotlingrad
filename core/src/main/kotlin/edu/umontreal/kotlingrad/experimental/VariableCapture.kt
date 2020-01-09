81d78216552bcb33d6ad4beedc9b7bf19b4fb322
breandan <bre@ndan.co>
2020-01-09 17:10:02 -0500

    readability improvements

--- core/src/main/kotlin/edu/umontreal/kotlingrad/experimental/VariableCapture.kt
+++ core/src/main/kotlin/edu/umontreal/kotlingrad/experimental/VariableCapture.kt
@@ -7,13 +7,13 @@ fun main() {
     val q = X + Y + Z + Y + 0.0
     println("q = $q") // Should print above expression
     val totalApp = q(X to 1.0, Y to 2.0, Z to 3.0) // Name resolution
-    println(totalApp) // Should be 8
+    println("Should be 8: $totalApp")
     val partialApp = q(X to 1.0, Y to 1.0)(Z to 1.0) // Currying is possible
-    println(partialApp) // Should be 4
+    println("Should be 4: $partialApp")
     val partialApp2 = q(X to 1.0)(Y to 1.0, Z to 1.0) // Any arity is possible
-    println(partialApp2) // Should be 4
+    println("Should be 4: $partialApp2")
     val partialApp3 = q(Z to 1.0)(X to 1.0, Y to 1.0) // Any order is possible
-    println(partialApp3) // Should be 4
+    println("Should be 4: $partialApp3")
 
     val t = X + Z / Z + Y + 0.0
     val v = t(Y to 4.0)
@@ -26,10 +26,10 @@ fun main() {
 
     val p = X + Y * Z + 0.0
     val totalApp2 = p(X to 1.0, Y to 2.0, Z to 3.0)
-    println(totalApp2) // Should be 7
+    println("Should be 7: $totalApp2")
     val d = X + Z * X
-    println(d(X to 3.0, Z to 4.0)) // Should be 15
-    println((2.0 * d)(X to 3.0, Z to 4.0)) // Should be 30
+    println("Should be 15: " + d(X to 3.0, Z to 4.0))
+    println("Should be 30: " + (2.0 * d)(X to 3.0, Z to 4.0))
   }
 }
 
@@ -556,10 +556,10 @@ class XYZ<P: Const<P, in Number>>(override val left: BiFn<*>,
 abstract class BiFn<T: Const<T, Number>>(open val left: BiFn<*>? = null,
                                          open val right: BiFn<*>? = null,
                                          open val op: Op<*>? = null) {
-  val add: Add<T> by lazy { Add<T>() }
-  val mul: Mul<T> by lazy { Mul<T>() }
-  val sub: Sub<T> by lazy { Sub<T>() }
-  val div: Div<T> by lazy { Div<T>() }
+  val add: Add<T> by lazy { Add() }
+  val mul: Mul<T> by lazy { Mul() }
+  val sub: Sub<T> by lazy { Sub() }
+  val div: Div<T> by lazy { Div() }
 
   override fun toString() = "$left $op $right"
 }
