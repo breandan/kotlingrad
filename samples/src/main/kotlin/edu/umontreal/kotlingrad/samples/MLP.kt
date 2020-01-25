6378d51e50ecc6233164bcda199cdc8ea418ca72
breandan <bre@ndan.co>
2020-01-25 01:41:08 -0500

    more MLP debugging

---
+++ samples/src/main/kotlin/edu/umontreal/kotlingrad/samples/MLP.kt
@@ -0,0 +1,72 @@
+package edu.umontreal.kotlingrad.samples
+
+import edu.umontreal.kotlingrad.experimental.*
+import kotlin.random.Random
+
+@Suppress("NonAsciiCharacters")
+class MLP<T: SFun<T>>(
+  val x: Var<T> = Var("x"),
+  val y: Var<T> = Var("y"),
+  val p1v: VVar<T, D3> = VVar(),
+  val p2v: MVar<T, D3, D3> = MVar(),
+  val p3v: VVar<T, D3> = VVar()
+) {
+  operator fun invoke(p1: VFun<T, D3>, p2: MFun<T, D3, D3>, p3: VFun<T, D3>) =
+    asFun()(p1v to p1)(p2v to p2)(p3v to p3)
+
+  fun asFun(): SFun<T> {
+    val layer1 = layer(p1v * x)
+    val layer2 = layer(p2v * layer1)
+    val output = layer2 dot p3v
+    val lossFun = (output - y) pow Two()
+    return lossFun
+  }
+
+  private fun layer(x: VFun<T, D3>): VFun<T, D3> = x.map { sigmoid(it) }
+}
+
+fun <T: SFun<T>> sigmoid(x: SFun<T>) = One<T>() / (One<T>() + E<T>().pow(-x))
+
+fun main() = with(DoublePrecision) {
+  var w1: VFun<DReal, D3> = Vec(1.0, 1.0, 1.0)
+
+  var w2: MFun<DReal, D3, D3> = Mat3x3(
+    1.0, 1.0, 1.0,
+    1.0, 1.0, 1.0,
+    1.0, 1.0, 1.0
+  )
+
+  var w3: VFun<DReal, D3> = Vec(1.0, 1.0, 1.0)
+
+  val oracle = { it: Double -> it * it }
+  val drawSample = { Random.nextDouble().let { Pair(it, oracle(it)) } }
+  val mlp = MLP<DReal>()
+  val mlpf = mlp.asFun()
+
+  var i = 1.0
+  var totalLoss = 0.0
+  val α = DReal(0.01)
+
+  do {
+    val (X, Y) = drawSample()
+    val t = mlp(w1, w2, w3)(mlp.x to X, mlp.y to Y)
+
+    totalLoss += t.toDouble()
+
+    val dw1 = mlpf.d(mlp.p1v)
+    val dw2 = mlpf.d(mlp.p2v)
+    val dw3 = mlpf.d(mlp.p3v)
+
+    val t1 = dw1(mlp.p1v to w1)(mlp.p2v to w2)(mlp.p3v to w3)(mlp.x to X, mlp.y to Y)
+    val t2 = dw2(mlp.p1v to w1)(mlp.p2v to w2)(mlp.p3v to w3)(mlp.x to X, mlp.y to Y)
+    val t3 = dw3(mlp.p1v to w1)(mlp.p2v to w2)(mlp.p3v to w3)(mlp.x to X, mlp.y to Y)
+    val s1 = α * t1
+    println(s1)
+    w1 += s1
+    w2 += α * t2
+    w3 += α * t3
+
+    val avgLoss = totalLoss / i
+    println(avgLoss)
+  } while (i++ < 100 || 0.5 < avgLoss)
+}
--- samples/src/main/kotlin/edu/umontreal/kotlingrad/samples/VisualizeDFG.kt
+++ samples/src/main/kotlin/edu/umontreal/kotlingrad/samples/VisualizeDFG.kt
@@ -3,36 +3,35 @@ package edu.umontreal.kotlingrad.samples
 import edu.umontreal.kotlingrad.experimental.DoublePrecision
 import edu.umontreal.kotlingrad.experimental.SFun
 import guru.nidi.graphviz.attribute.*
-import guru.nidi.graphviz.attribute.Rank.RankDir.LEFT_TO_RIGHT
-import guru.nidi.graphviz.edge
+import guru.nidi.graphviz.attribute.Rank.RankDir.*
+import guru.nidi.graphviz.*
 import guru.nidi.graphviz.engine.Format.SVG
-import guru.nidi.graphviz.graph
-import guru.nidi.graphviz.node
-import guru.nidi.graphviz.toGraphviz
+import guru.nidi.graphviz.engine.Renderer
 import java.io.File
 
 fun main() {
   with(DoublePrecision) {
     val t = (1 + x * 2 - 3 + y + z / y).d(y).d(x) + z / y * 3 - 2
-    t.render("$resourcesPath/dataflow.svg").viewInBrowser()
+    t.render().saveToFile("dataflow.svg").viewInBrowser()
   }
 }
 
 const val DARKMODE = false
 const val THICKNESS = 2
 
-fun SFun<*>.render(filename: String) =
+fun SFun<*>.render() =
   graph(directed = true) {
     val color = if (DARKMODE) Color.WHITE else Color.BLACK
 
     edge[color, Arrow.NORMAL, Style.lineWidth(THICKNESS)]
 
-    graph[Rank.dir(LEFT_TO_RIGHT), Color.TRANSPARENT.background()]
+    graph[Rank.dir(TOP_TO_BOTTOM), Color.TRANSPARENT.background()]
 
     node[color, color.font(), Font.config("Helvetica", 20),
       Style.lineWidth(THICKNESS)]
 
     toGraph()
-  }.toGraphviz().render(SVG).run {
-    toFile(File(filename))
-  }
+  }.toGraphviz().render(SVG)
+
+fun Renderer.saveToFile(filename: String) =
+  toFile(File("$resourcesPath/$filename"))
