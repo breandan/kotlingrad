a5ec1db26a977b4f596b46ac3b06fc0a1038fbfc
breandan <bre@ndan.co>
2020-01-26 23:57:02 -0500

    lift context-based type conversion into RealNumber

--- core/src/main/kotlin/edu/umontreal/kotlingrad/experimental/ToyExtras.kt
+++ core/src/main/kotlin/edu/umontreal/kotlingrad/experimental/ToyExtras.kt
@@ -4,7 +4,7 @@ import ch.obermuhlner.math.big.BigDecimalMath
 import java.math.BigDecimal
 import java.math.MathContext
 
-class BDReal(number: Number, val sigFigs: Int = 30): RealNumber<BDReal, BigDecimal>(when {
+open class BDReal(number: Number, val sigFigs: Int = 30): RealNumber<BDReal, BigDecimal>(when {
   number is BigDecimal -> number
   number.toDouble().isNaN() -> BigDecimal.ZERO
   1E30 < number.toDouble() -> BigDecimal(1E30)
@@ -12,7 +12,14 @@ class BDReal(number: Number, val sigFigs: Int = 30): RealNumber<BDReal, BigDecim
   else -> BigDecimal(number.toDouble() + 0.0)
 }) {
   val mc = MathContext(sigFigs)
-  override fun wrap(value: Number) = BDReal(value, sigFigs)
+
+  companion object: BDReal(BigDecimal.ZERO)
+
+  override fun wrap(number: Any) = when (number) {
+    is BigDecimal -> BDReal(number, number.precision())
+    is Number -> BDReal(number.toDouble(), sigFigs)
+    else -> BDReal(number.toString().toDouble(), sigFigs)
+  }
 
   override fun sin() = BDReal(BigDecimalMath.sin(value, mc))
   override fun cos() = BDReal(BigDecimalMath.cos(value, mc))
@@ -25,12 +32,14 @@ class BDReal(number: Number, val sigFigs: Int = 30): RealNumber<BDReal, BigDecim
     is BDReal -> BDReal(value + addend.value)
     else -> super.plus(addend)
   }
+
   override fun times(multiplicand: SFun<BDReal>) = when (multiplicand) {
     is BDReal -> BDReal(value * multiplicand.value)
     else -> super.times(multiplicand)
   }
-  override fun pow(exp: SFun<BDReal>) = when(exp) {
-    is BDReal -> BDReal(BigDecimalMath.pow(value, exp.value, mc))
-    else -> super.pow(exp)
+
+  override fun pow(exponent: SFun<BDReal>) = when (exponent) {
+    is BDReal -> BDReal(BigDecimalMath.pow(value, exponent.value, mc))
+    else -> super.pow(exponent)
   }
 }
