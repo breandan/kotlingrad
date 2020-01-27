0108ae860526d74be59612f7bad0c3f327aae06b
breandan <bre@ndan.co>
2020-01-27 00:31:14 -0500

    update initialization and logging

--- samples/src/main/kotlin/edu/umontreal/kotlingrad/samples/MLP.kt
+++ samples/src/main/kotlin/edu/umontreal/kotlingrad/samples/MLP.kt
@@ -28,16 +28,17 @@ class MLP<T: SFun<T>>(
 fun <T: SFun<T>> sigmoid(x: SFun<T>) = One<T>() / (One<T>() + E<T>().pow(-x))
 
 fun main() = with(DoublePrecision) {
-  var w1: VFun<DReal, D3> = Vec(D3) { 1.0 }
-  var w2: MFun<DReal, D3, D3> = Mat(D3, D3) { 1.0 }
-  var w3: VFun<DReal, D3> = Vec(D3) { 1.0 }
+  val rand = java.util.Random()
+  var w1: VFun<DReal, D3> = Vec(D3) { rand.nextDouble() }
+  var w2: MFun<DReal, D3, D3> = Mat(D3, D3) { rand.nextDouble() }
+  var w3: VFun<DReal, D3> = Vec(D3) { rand.nextDouble() }
 
   val oracle = { it: Double -> it * it }
   val drawSample = { Random.nextDouble().let { Pair(it, oracle(it)) } }
   val mlp = MLP<DReal>()
   val mlpf = mlp.asFun()
 
-  var i = 1.0
+  var epochs = 1
   var totalLoss = 0.0
   val α = wrap(0.01)
 
@@ -45,7 +46,7 @@ fun main() = with(DoublePrecision) {
     val (X, Y) = drawSample()
     val t = mlp(w1, w2, w3)(mlp.x to X, mlp.y to Y)
 
-    val avgLoss = t.toDouble() / 100
+    totalLoss += t.toDouble()
 
     val dw1 = mlpf.d(mlp.p1v)
     val dw2 = mlpf.d(mlp.p2v)
@@ -54,12 +55,14 @@ fun main() = with(DoublePrecision) {
     val t1 = dw1(mlp.p1v to w1)(mlp.p2v to w2)(mlp.p3v to w3)(mlp.x to X, mlp.y to Y)()
     val t2 = dw2(mlp.p1v to w1)(mlp.p2v to w2)(mlp.p3v to w3)(mlp.x to X, mlp.y to Y)()
     val t3 = dw3(mlp.p1v to w1)(mlp.p2v to w2)(mlp.p3v to w3)(mlp.x to X, mlp.y to Y)()
-    val s1 = α * t1
-    println(s1)
-    w1 += s1
+
+    w1 += α * t1
     w2 += α * t2
     w3 += α * t3
 
-    println(avgLoss)
-  } while (i++ < 10000 || 0.5 < avgLoss)
+    if(epochs % 10 == 0){
+      println("Average loss at ${epochs / 10} epochs: ${totalLoss / 100}")
+      totalLoss = 0.0
+    }
+  } while (epochs++ < 1000)
 }
