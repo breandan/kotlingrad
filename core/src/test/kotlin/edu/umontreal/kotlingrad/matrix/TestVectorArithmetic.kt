8c19de29dc51f65cf2b510db08c52d0a441a9ee9
breandan <bre@ndan.co>
2020-02-04 00:22:08 -0500

    more vector arithmetic tests

--- core/src/test/kotlin/edu/umontreal/kotlingrad/matrix/TestVectorArithmetic.kt
+++ core/src/test/kotlin/edu/umontreal/kotlingrad/matrix/TestVectorArithmetic.kt
@@ -5,6 +5,7 @@ import edu.umontreal.kotlingrad.experimental.DoublePrecision
 import edu.umontreal.kotlingrad.shouldBeAbout
 import io.kotlintest.properties.assertAll
 import io.kotlintest.specs.StringSpec
+import kotlin.math.pow
 
 @Suppress("NonAsciiCharacters", "LocalVariableName")
 class TestVectorArithmetic: StringSpec({
@@ -17,7 +18,37 @@ class TestVectorArithmetic: StringSpec({
         val d = Vec(v3, v4)
         val c = a ʘ b
         val s = c(b to d)
+        s shouldBeAbout Vec(v1 * v3, v2 * v4)
+      }
+    }
+
+    "a + b" {
+      gen.assertAll { v1: Double, v2: Double, v3: Double, v4: Double ->
+        val a = Vec(v1, v2)
+        val b = Var2("t")
+        val d = Vec(v3, v4)
+        val c = a + b
+        val s = c(b to d)
         s shouldBeAbout Vec(v1 + v3, v2 + v4)
+      }
+    }
+
+    "sum contents" {
+      gen.assertAll { v1: Double, v2: Double, v3: Double, v4: Double ->
+        val a = Vec(v1, v2, v3, v4)
+        val b = Var4("t")
+        val c = b.sum()
+        val s = c(b to a)
+        s shouldBeAbout v1 + v2 + v3 + v4
+      }
+    }
+
+    "magnitude" {
+      gen.assertAll { v1: Double, v2: Double, v3: Double, v4: Double ->
+        val a = Vec(v1, v2, v3, v4)
+        val b = Var4("t") * 1
+        val s = b(b to a)
+        s.magnitude() shouldBeAbout kotlin.math.sqrt(v1.pow(2) + v2.pow(2) + v3.pow(2) + v4.pow(2))
       }
     }
   }
