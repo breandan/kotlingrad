817e1b848766c599663a7679e12596ca4c9090bb
breandan <bre@ndan.co>
2020-06-20 21:37:19 -0400

    restyle DFG using graphviz Mrecords

--- samples/src/main/kotlin/edu/umontreal/kotlingrad/samples/VisualizeDFG.kt
+++ samples/src/main/kotlin/edu/umontreal/kotlingrad/samples/VisualizeDFG.kt
@@ -3,6 +3,7 @@ package edu.umontreal.kotlingrad.samples
 import edu.umontreal.kotlingrad.experimental.DoublePrecision
 
 fun main() = with(DoublePrecision) {
-  val t = (1 + x * 2 + z / y).d(y).d(x) + z / y * 3 - 4 * (y pow y).d(y)
-  t.saveToFile("dataflow.dot")
+  val t = (1 + x * 2 - 3 + y + z / y).d(y).d(x) + z / y * 3 - 2
+//  val t = (1 + x * 2 + z / y).d(y).d(x) + z / y * 3 - 4 * (y pow y).d(y)
+  t.saveToFile("dataflow.svg")
 }
