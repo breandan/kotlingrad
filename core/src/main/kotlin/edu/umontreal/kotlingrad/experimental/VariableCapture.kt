b496ecd08f66ee66cc410e8f85027c3b85505a00
breandan <bre@ndan.co>
2020-08-02 16:34:51 -0400

    higher-arity function combinators

--- core/src/main/kotlin/edu/umontreal/kotlingrad/experimental/VariableCapture.kt
+++ core/src/main/kotlin/edu/umontreal/kotlingrad/experimental/VariableCapture.kt
@@ -15,7 +15,7 @@ fun main() {
     val partialApp3 = q(Z to 1.0)(X to 1.0, Y to 1.0) // Any order is possible
     println("Should be 4: $partialApp3")
 
-    val t = X + Z / Z + Y + 0.0
+    val t = (X + Z) / (Z + Y + 0.0)
     val v = t(Y to 4.0)
     val l = t(X to 1.0)(Z to 2.0)
     val r = t(X to 1.0)(Z to 2.0)(Y to 3.0) // Full currying
