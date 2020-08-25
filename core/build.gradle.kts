ee62e8632ea885eb7946a87faf5aaeca91ca669d
breandan.considine <breandan.considine@gmail.com>
2020-08-25 05:46:15 -0400

    restore maven build, shouldn't have been deleted

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -57,3 +57,42 @@ tasks {
     doLast { logger.info("Kotlin∇ notebook was installed in: $installPath") }
   }
 }
+
+val fatJar by tasks.creating(Jar::class) {
+  archiveBaseName.set("${project.name}-fat")
+  manifest {
+    attributes["Implementation-Title"] = "kotlingrad"
+    attributes["Implementation-Version"] = archiveVersion
+  }
+  from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+  with(tasks.jar.get() as CopySpec)
+}
+
+publishing {
+  publications.create<MavenPublication>("default") {
+    artifact(fatJar)
+    pom {
+      description.set("Kotlin∇: Differentiable Functional Programming with Algebraic Data Types")
+      name.set("Kotlin∇")
+      url.set("https://github.com/breandan/kotlingrad")
+      licenses {
+        license {
+          name.set("The Apache Software License, Version 1.0")
+          url.set("http://www.apache.org/licenses/LICENSE-3.0.txt")
+          distribution.set("repo")
+        }
+      }
+      developers {
+        developer {
+          id.set("Breandan Considine")
+          name.set("Breandan Considine")
+          email.set("bre@ndan.co")
+          organization.set("Université de Montréal")
+        }
+      }
+      scm {
+        url.set("https://github.com/breandan/kotlingrad")
+      }
+    }
+  }
+}
