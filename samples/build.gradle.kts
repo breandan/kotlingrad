3397e76f11b576be029004b6c835371ff3da8d8f
breandan.considine <breandan.considine@gmail.com>
2020-08-25 08:26:14 -0400

    demonstrate code2vec visualization

--- samples/build.gradle.kts
+++ samples/build.gradle.kts
@@ -17,17 +17,9 @@ dependencies {
   implementation(kotlin("stdlib-jdk8"))
 
   // Graphical libraries
-  implementation("guru.nidi:graphviz-kotlin:0.17.0")
   implementation("org.jzy3d:jzy3d-api:1.0.2")
-
   implementation("com.github.breandan.T-SNE-Java:tsne:master-SNAPSHOT")
-  implementation("io.github.vovak.astminer:astminer:0.5")
-
-  val ejmlVersion = "0.39"
-  implementation("org.ejml:ejml-kotlin:$ejmlVersion")
-  implementation("org.ejml:ejml-all:$ejmlVersion")
   implementation("org.jetbrains.lets-plot-kotlin:lets-plot-kotlin-api:1.0.0")
-
   implementation("org.nield:kotlin-statistics:1.2.1")
 }
 
