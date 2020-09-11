472913b2ba7543e1de58f7ca019d137a9c34839f
breandan <bre@ndan.co>
2020-09-10 20:02:21 -0400

    minor version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 
 plugins {
   idea
-  kotlin("jvm") version "1.4.0" // Keep in sync with README
+  kotlin("jvm") version "1.4.10" // Keep in sync with README
 }
 
 allprojects {
