88218bf26b2541dab2dbf8e4a68e0ba9f0f54f19
breandan <bre@ndan.co>
2020-10-31 01:19:51 -0400

    fix broken CI build

--- core/src/test/kotlin/edu/umontreal/kotlingrad/ExpressionGenerator.kt
+++ core/src/test/kotlin/edu/umontreal/kotlingrad/ExpressionGenerator.kt
@@ -1,12 +1,13 @@
 package edu.umontreal.kotlingrad
 
+import edu.mcgill.kaliningraph.DEFAULT_RANDOM
 import edu.umontreal.kotlingrad.api.*
 import io.kotlintest.properties.Gen
 import io.kotlintest.properties.shrinking.Shrinker
 import kotlin.math.pow
 
 class TestExpressionGenerator<X : RealNumber<X, *>>(proto: X) :
-  ExpressionGenerator<X>(seededRandom), Gen<SFun<X>> {
+  ExpressionGenerator<X>(DEFAULT_RANDOM), Gen<SFun<X>> {
   val constants = List(100) { SConst<X>(rand.nextDouble() * 10.0.pow(rand.nextInt(5))) }
 
   override fun constants(): Iterable<SFun<X>> = constants + variables
