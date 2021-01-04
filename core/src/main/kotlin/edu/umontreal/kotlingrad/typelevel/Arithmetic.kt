789b7f78311529cabed9f22bccbda3637ac7114a
breandan <bre@ndan.co>
2021-01-04 16:39:31 -0500

    implement typeclass-based nats

--- core/src/main/kotlin/edu/umontreal/kotlingrad/typelevel/Arithmetic.kt
+++ core/src/main/kotlin/edu/umontreal/kotlingrad/typelevel/Arithmetic.kt
@@ -1,12 +1,12 @@
 package edu.umontreal.kotlingrad.typelevel
 
-interface Nat { val eval: Int }
+interface TLNat { val eval: Int }
 
-abstract class Suc<T: Nat>(val succ: T): Nat {
+abstract class Suc<T: TLNat>(val succ: T): TLNat {
   override val eval: Int = succ.eval + 1
 }
 
-object D0: Nat { override val eval: Int = 0 }
+object D0: TLNat { override val eval: Int = 0 }
 typealias N1 = Suc<D0>; object D1: N1(D0)
 typealias N2 = Suc<N1>; object D2: N2(D1)
 typealias N3 = Suc<N2>; object D3: N3(D2)
@@ -17,18 +17,18 @@ typealias N7 = Suc<N6>; object D7: N7(D6)
 typealias N8 = Suc<N7>; object D8: N8(D7)
 typealias N9 = Suc<N8>; object D9: N9(D8)
 
-class Sum<L: Nat, R: Nat>(left: L, right: R): Nat {
+class Sum<L: TLNat, R: TLNat>(left: L, right: R): TLNat {
   override val eval: Int = left.eval + right.eval
 }
 
-class Pdt<L: Nat, R: Nat>(left: L, right: R): Nat {
+class Pdt<L: TLNat, R: TLNat>(left: L, right: R): TLNat {
   override val eval: Int = left.eval * right.eval
 }
 
 inline fun <reified S1: N3> Vec<S1>.takesThree() = size
 fun takesFour(four: N4): N7 = D7
 
-class Vec<O: Nat>(o: O) { val size = o.eval }
+class Vec<O: TLNat>(o: O) { val size = o.eval }
 
 @JvmName("1+1=2") operator fun N1.plus(n: N1): N2 = D2
 @JvmName("2+2=4") operator fun N2.plus(n: N2): N4 = D4
--- core/src/main/kotlin/edu/umontreal/kotlingrad/typelevel/TypeClassing.kt
+++ core/src/main/kotlin/edu/umontreal/kotlingrad/typelevel/TypeClassing.kt
@@ -1,55 +1,75 @@
 package edu.umontreal.kotlingrad.typelevel
 
 import java.math.BigInteger.*
+import kotlin.system.measureTimeMillis
 
-// https://kotlin.christmas/2020/7
-interface Addable<T> {
-  val one: T
-  val nil: T
-  operator fun T.plus(t: T): T
+fun main() {
+  Semiring(
+    nil = ZERO,
+    one = ONE,
+    plus = { a, b -> a + b },
+    times = { a, b -> a * b }
+  ).run {
+    measureTimeMillis {
+      printFibNum(valueOf(20))
+      printPrimes(valueOf(20))
+    }.also { ms -> println("Took ${ms}ms") }
+  }
 
-  companion object {
-    operator fun <T> invoke(nil: T, one: T, plus: (T, T) -> T): Addable<T> =
-      object: Addable<T> {
-        override fun T.plus(t: T) = plus(this, t)
-        override val nil: T = nil
-        override val one: T = one
-      }
+  Semiring(
+    nil = ZERO,
+    one = ONE,
+    plus = { a, b -> a + b }
+  ).run {
+    measureTimeMillis {
+      printFibNum(valueOf(20))
+      printPrimes(valueOf(20))
+    }.also { ms -> println("Took ${ms}ms") }
+  }
+
+  Nat(
+    nil = ZERO,
+    succ = { a -> a + ONE }
+  ).run {
+    measureTimeMillis {
+      printFibNum(valueOf(20))
+      printPrimes(valueOf(20))
+    }.also { ms -> println("Took ${ms}ms") }
   }
 }
 
-/** Corecursive Fibonacci sequence of [Addable]s **/
-tailrec fun <T> Addable<T>.fibCorec(
+/** Corecursive Fibonacci sequence of [Nat]s **/
+tailrec fun <T> Nat<T>.printFibNum(
   n: T,
   seed: Pair<T, T> = nil to one,
   fib: (Pair<T, T>) -> Pair<T, T> = { (a, b) -> b to a + b },
   i: T = nil,
 ): T =
   if (i == n) fib(seed).first
-  else fibCorec(n = n, seed = fib(seed), i = i + one)
+  else printFibNum(
+    n = n,
+    seed = fib(seed).also { print("${it.first},") },
+    i = i + one
+  )
 
-/** Returns the product of two [Addable]s **/
-tailrec fun <T> Addable<T>.times(l: T, r: T, acc: T = nil, i: T = nil): T =
-  if (i == r) acc else times(l, r, acc + l, i + one)
-
-/** Returns a sequence of [Addable]s starting with [from] until [to] **/
-tailrec fun <T> Addable<T>.seq(
+/** Returns a sequence of [Nat]s starting from [from] until [to] **/
+tailrec fun <T> Nat<T>.seq(
   from: T = one,
   to: T,
   acc: Set<T> = emptySet()
 ): Set<T> =
   if (from == to) acc else seq(from + one, to, acc + from)
 
-/** Returns whether an [Addable] is prime **/
-fun <T> Addable<T>.isPrime(t: T, kps: Set<T> = emptySet()): Boolean =
-  // Take Cartesian product, take only distinct pairs due to commutativity
+/** Returns whether an [Nat] is prime **/
+fun <T> Nat<T>.isPrime(t: T, kps: Set<T> = emptySet()): Boolean =
+  // Take Cartesian product, filter distinct pairs due to commutativity
   (if (kps.isNotEmpty()) kps * kps
   else seq(to = t) * seq(to = t))
     .distinctBy { (l, r) -> setOf(l, r) }
-    .all { (i, j) -> if (i == one || j == one) true else times(i, j) != t }
+    .all { (i, j) -> if (i == one || j == one) true else i * j != t }
 
-/** Prints [t] prime [Addable]s **/
-tailrec fun <T> Addable<T>.printPrimes(
+/** Prints [t] prime [Nat]s **/
+tailrec fun <T> Nat<T>.printPrimes(
   t: T, // number of primes
   i: T = nil, // counter
   c: T = one + one, // prime candidate
@@ -65,13 +85,57 @@ tailrec fun <T> Addable<T>.printPrimes(
 
 // Returns the Cartesian product of two sets
 operator fun <T> Set<T>.times(s: Set<T>) =
-  flatMap { l -> s.map { r -> l to r } }
+  flatMap { l -> s.map { r -> l to r }.toSet() }.toSet()
 
-fun main() {
-  Addable(ZERO, ONE) { a, b -> a + b }.run {
-    val q = fibCorec(valueOf(100))
-    println(q)
-    println(times(valueOf(5), valueOf(5)))
-    printPrimes(valueOf(10))
+/** Returns the product of two [Nat]s **/
+tailrec fun <T> Nat<T>.times(l: T, r: T, acc: T = nil, i: T = nil): T =
+  if (i == r) acc else times(l, r, acc + l, i + one)
+
+/** Returns the sum of two [Nat]s **/
+tailrec fun <T> Nat<T>.plus(l: T, r: T, acc: T = l, i: T = nil): T =
+  if (i == r) acc else plus(l, r, succ(acc), succ(i))
+
+fun <T> Nat<T>.sum(list: List<T>): T = list.reduce { acc, t -> acc + t }
+
+interface Nat<T> {
+  val nil: T
+  val one: T
+    get() = succ(nil)
+
+  fun succ(t: T): T
+  operator fun T.plus(t: T) = plus(this, t)
+  operator fun T.times(t: T) = times(this, t)
+
+  companion object {
+    operator fun <T> invoke(nil: T, succ: (T) -> T): Nat<T> =
+      object: Nat<T> {
+        override val nil: T = nil
+        override fun succ(t: T): T = succ(t)
+      }
+  }
+}
+
+interface Semiring<T>: Nat<T> {
+  override fun succ(t: T): T = t + one
+
+  companion object {
+    operator fun <T> invoke(nil: T, one: T, plus: (T, T) -> T): Semiring<T> =
+      object: Semiring<T> {
+        override fun T.plus(t: T) = plus(this, t)
+        override val nil: T = nil
+        override val one: T = one
+      }
+
+    operator fun <T> invoke(
+      nil: T, one: T,
+      plus: (T, T) -> T,
+      times: (T, T) -> T
+    ): Semiring<T> =
+      object: Semiring<T> {
+        override fun T.plus(t: T) = plus(this, t)
+        override fun T.times(t: T) = times(this, t)
+        override val nil: T = nil
+        override val one: T = one
+      }
   }
 }
