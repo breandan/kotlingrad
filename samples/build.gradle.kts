c812c99ab7a8895416c8a1f88456c41a9a361f80
breandan <bre@ndan.co>
2021-02-14 01:57:55 -0500

    update deps

--- samples/build.gradle.kts
+++ samples/build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
 
   // Graphical libraries
   implementation("org.jzy3d:jzy3d-api:1.0.3")
-  implementation("com.github.lejon.T-SNE-Java:tsne:master-SNAPSHOT")
+  implementation("com.github.breandan.T-SNE-Java:tsne:master-SNAPSHOT")
   implementation("org.jetbrains.lets-plot-kotlin:lets-plot-kotlin-api:1.2.0")
   implementation("io.github.vovak.astminer:astminer:0.6")
   implementation("org.nield:kotlin-statistics:1.2.1")
