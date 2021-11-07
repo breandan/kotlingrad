7f73dadb4eaba2de6ac6fda6b5277577af1a6b3e
breandan <breandan.considine@mail.mcgill.ca>
2021-11-07 01:40:38 -0400

    common sources should be standalone

--- shipshape/src/main/kotlin/ai/hypergraph/shipshape/DimGen.kt
+++ shipshape/src/main/kotlin/ai/hypergraph/shipshape/DimGen.kt
@@ -9,7 +9,7 @@ const val maxNat = 100
 @Language("kt")
 fun genDim() = """
   interface Nat<T: D0> { val i: Int }
-  sealed class INat<T: D0>(open val i: Int): SConst<INat<T>>(i) { override fun toString() = "${"\$"}i" }
+  sealed class INat<T: D0>(open val i: Int) { override fun toString() = "${"\$"}i" }
   
   sealed class D0(override val i: Int = 0): INat<D0>(i) { companion object: D0(), Nat<D0> }
   sealed class DN(): D1() { companion object: DN(), Nat<DN> }
