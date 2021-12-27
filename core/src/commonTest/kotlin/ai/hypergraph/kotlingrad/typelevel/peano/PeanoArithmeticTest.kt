d05c7f30723b7ab85aa8d16c58c8680cdf449a32
breandan <bre@ndan.co>
2021-12-27 02:45:36 -0500

    more granular unit tests

--- core/src/commonTest/kotlin/ai/hypergraph/kotlingrad/typelevel/peano/PeanoArithmeticTest.kt
+++ core/src/commonTest/kotlin/ai/hypergraph/kotlingrad/typelevel/peano/PeanoArithmeticTest.kt
@@ -4,13 +4,19 @@ import kotlin.test.*
 
 class PeanoArithmeticTest {
   @Test
-  fun basicTest() {
+  fun shouldBeFour() {
     val four = S2 + S2
     assertEquals(4, four.toInt())
-    
-    val sixteen = four * four
-    assertEquals(16, sixteen.toInt())
+  }
 
+  @Test
+  fun shouldBeSixteen() {
+    val sixteen = S4 * S4
+    assertEquals(16, sixteen.toInt())
+  }
+
+  @Test
+  fun shouldBeFive() {
     val five = O
       .plus2()
       .let { it + S3 }
@@ -30,5 +36,32 @@ class PeanoArithmeticTest {
       .let { it + S3 }
 
     assertEquals(5, five.toInt())
+  }
+
+  @Test
+  fun shouldBeTwo() {
+    val two = S1
+      .plus1()
+      .plus1()
+      .plus1()
+      .plus1()
+      .plus1()
+      .plus1()
+      .plus1()
+      .minus2()
+      .minus2()
+      .minus2()
+      .minus2()
+      .plus2()
+    
+
+    assertEquals(2, two.toInt())
+  }
+  
+  @Test
+  fun shouldBeThree() {
+    val three = S2 * S2 * S2 * S2 / S4 * S3 / S6 + S3 - S2
+
+    assertEquals(3, three.toInt())
   }
 }
