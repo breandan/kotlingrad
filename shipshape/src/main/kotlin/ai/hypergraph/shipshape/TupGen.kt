f529c1d770ae5c46ea7a54dd29b6165db4e99365
breandan <bre@ndan.co>
2022-01-02 05:58:06 -0500

    prototype shapesafe array indexing

--- shipshape/src/main/kotlin/ai/hypergraph/shipshape/TupGen.kt
+++ shipshape/src/main/kotlin/ai/hypergraph/shipshape/TupGen.kt
@@ -17,23 +17,13 @@ fun genTuple() =
 
     data class T6<A1, A2, A3, A4, A5, A6>(val t1: A1, val t2: A2, val t3: A3, val t4: A4, val t5: A5, val t6: A6)
 
-    infix fun <A1, A2> A1.cc(a2: A2): T2<A1, A2> {
-        return T2(this, a2)
-    }
+    infix fun <A1, A2> A1.cc(a2: A2): T2<A1, A2> = T2(this, a2)
 
-    infix fun <A1, A2, A3> T2<A1, A2>.cc(a3: A3): T3<A1, A2, A3> {
-        return T3(this.t1, this.t2, a3)
-    }
+    infix fun <A1, A2, A3> T2<A1, A2>.cc(a3: A3): T3<A1, A2, A3> = T3(t1, t2, a3)
 
-    infix fun <A1, A2, A3, A4> T3<A1, A2, A3>.cc(a4: A4): T4<A1, A2, A3, A4> {
-        return T4(this.t1, this.t2, this.t3, a4)
-    }
+    infix fun <A1, A2, A3, A4> T3<A1, A2, A3>.cc(a4: A4): T4<A1, A2, A3, A4> = T4(t1, t2, t3, a4)
 
-    infix fun <A1, A2, A3, A4, A5> T4<A1, A2, A3, A4>.cc(a5: A5): T5<A1, A2, A3, A4, A5> {
-        return T5(this.t1, this.t2, this.t3, this.t4, a5)
-    }
+    infix fun <A1, A2, A3, A4, A5> T4<A1, A2, A3, A4>.cc(a5: A5): T5<A1, A2, A3, A4, A5> = T5(t1, t2, t3, t4, a5)
 
-    infix fun <A1, A2, A3, A4, A5, A6> T5<A1, A2, A3, A4, A5>.cc(a6: A6): T6<A1, A2, A3, A4, A5, A6> {
-        return T6(this.t1, this.t2, this.t3, this.t4, this.t5, a6)
-    }
+    infix fun <A1, A2, A3, A4, A5, A6> T5<A1, A2, A3, A4, A5>.cc(a6: A6): T6<A1, A2, A3, A4, A5, A6> = T6(t1, t2, t3, t4, t5, a6)
   """.trimIndent()
