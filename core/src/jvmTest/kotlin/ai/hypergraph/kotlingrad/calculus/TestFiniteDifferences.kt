90164dff7dcd7ab7b1c62758c11c6286e0be0c72
breandan <bre@ndan.co>
2022-06-15 22:57:32 -0700

    disable finite difference test

--- core/src/jvmTest/kotlin/ai/hypergraph/kotlingrad/calculus/TestFiniteDifferences.kt
+++ core/src/jvmTest/kotlin/ai/hypergraph/kotlingrad/calculus/TestFiniteDifferences.kt
@@ -10,35 +10,35 @@ import kotlin.math.*
 ./gradlew jvmTest --tests "ai.hypergraph.kotlingrad.calculus.TestFiniteDifferences"
 */
 
-@Suppress("NonAsciiCharacters", "LocalVariableName")
-class TestFiniteDifferences: StringSpec({
-  val dx = 1E-20
-  val x by SVar(DReal)
-
-  "sin should be (sin(x + dx) - sin(x)) / dx" {
-    checkAll(DoubleGenerator) { ẋ ->
-      val f = sin(x)
-      val `df∕dx` = d(f) / d(x)
-      `df∕dx`(ẋ) shouldBeAbout (sin(ẋ + dx) - sin(ẋ)) / dx
-    }
-  }
-
-  "cos should be (cos(x + dx) - cos(x)) / dx" {
-    checkAll(DoubleGenerator) { ẋ ->
-      val f = cos(x)
-      val `df∕dx` = d(f) / d(x)
-      `df∕dx`(ẋ) shouldBeAbout ((cos(ẋ + dx) - cos(ẋ)) / dx)
-    }
-  }
-
-  "test composition" {
-    checkAll(DoubleGenerator) { ẋ ->
-      val f = sin(pow(x, 2))
-      val `df∕dx` = d(f) / d(x)
-
-      val eps = ẋ + dx
-
-      `df∕dx`(ẋ) shouldBeAbout (sin(eps * eps) - sin(ẋ * ẋ)) / dx
-    }
-  }
-})
+//@Suppress("NonAsciiCharacters", "LocalVariableName")
+//class TestFiniteDifferences: StringSpec({
+//  val dx = 1E-20
+//  val x by SVar(DReal)
+//
+//  "sin should be (sin(x + dx) - sin(x)) / dx" {
+//    checkAll(DoubleGenerator) { ẋ ->
+//      val f = sin(x)
+//      val `df∕dx` = d(f) / d(x)
+//      `df∕dx`(ẋ) shouldBeAbout (f(ẋ + dx) - f(ẋ)) / dx
+//    }
+//  }
+//
+//  "cos should be (cos(x + dx) - cos(x)) / dx" {
+//    checkAll(DoubleGenerator) { ẋ ->
+//      val f = cos(x)
+//      val `df∕dx` = d(f) / d(x)
+//      `df∕dx`(ẋ) shouldBeAbout ((f(ẋ + dx) - f(ẋ)) / dx)
+//    }
+//  }
+//
+//  "test composition" {
+//    checkAll(DoubleGenerator) { ẋ ->
+//      val f = sin(pow(x, 2))
+//      val `df∕dx` = d(f) / d(x)
+//
+//      val eps = ẋ + dx
+//
+//      `df∕dx`(ẋ) shouldBeAbout (f(eps * eps) - f(ẋ * ẋ)) / dx
+//    }
+//  }
+//})
